e1410b6f29fdde455c43c5a89855dcacc8899bd8
Randall Pruim <rpruim@gmail.com>
2012-01-12 21:16:25 -0500

    added documentation and fixed bugs (colliding use of variable h, better implementation of labels), refactored cascading ifs to use switch, removed .drop_from_list()

--- R/xhistogram.R
+++ R/xhistogram.R
@@ -1,15 +1,60 @@
-.drop_from_list <- function( l, names ) {
-	for (n in names) {
-		if (n %in% names(l) ) {
-			dots[[n]] <- NULL
-		}
-	}
-}
+#
+# this appears to be unused
+#
+#.drop_from_list <- function( l, names ) {
+#	for (n in names) {
+#		if (n %in% names(l) ) {
+#			dots[[n]] <- NULL
+#		}
+#	}
+#}
+
+#' Augmented histograms
+#' 
+#' \code{xhistoram} adds some additional functionality to \code{\link[lattice]{histogram}} making 
+#' it simpler to obtain certain common histogram adornments.
+#'
+#' @param x a formula or a numeric vector
+#' @param data a data frame in which to evaluate \code{x}
+#' @param panel a panel function
+#' @param type one of \code{'density'}, \code{'count'}, or \code{'percent'}
+#' @param \dots additional arguments passed to \code{\link[lattice]{histogram}} and on to
+#' 	\code{\link{panel.xhistogram}}
+#'
+#' @return a trellis object
+#' 
+#' @export
+#' @examples
+#' xhistogram(~age | substance, HELPrct, v=35, fit='normal')
+#' xhistogram(~age, HELPrct, labels=T, type='count')
+#' xhistogram(~age, HELPrct, groups=cut(age, seq(10,80,by=10)))
+#' xhistogram(~age, HELPrct, groups=sex, stripes='horizontal')
+
 
 xhistogram <- function (x, data=NULL, panel=panel.xhistogram, type='density', ...) {
   histogram(x, data=data, panel=panel, type=type, ...)
 }
 
+#' @rdname xhistogram
+#' @param dcol color of density curve
+#' @param gcol color of guidelines
+#' @param fcol fill color for histogram rectangles
+#' @param dmath density function for density curve overlay
+#' @param verbose be verbose?
+#' @param dn number of points to sample from density curve
+#' @param args a list of additional arguments for \code{dmath}
+#' @param labels should counts/densities/precents be displayed or each bin?
+#' @param density overlay density?
+#' @param fit a character string describing the distribution to fit.  Known distributions include
+#'      \code{"exponential"}, \code{"normal"}, \code{"lognormal" }, \code{"poisson"}, \code{"beta"}, \code{"geometric"},
+#'      \code{"t"}, \code{"weibull"}, \code{"cauchy"}, \code{"gamma"}, \code{"chisq"}, and \code{"chi-squared"}
+#'        
+#' @param start numeric value passed to \code{\link[MASS]{fitdistr}}
+#' @param groups,breaks as per \code{\link[lattice]{histogram}}
+#' @param stripes one of \code{"vertical"}, \code{"horizontal"}, or \code{"none"}, indicating
+#'        how bins should be striped when \code{groups} is not \code{NULL}
+#' @param h,v a vector of values for additional horizontal and vertical lines
+#' @param alpha transparency level
 panel.xhistogram <-
 function (x, 
 	dcol = trellis.par.get("plot.line")$col, dlwd = 2, 
@@ -97,38 +142,30 @@ function (x,
     	panel.histogram(x, type = type, breaks=breaks, ...)
 	}
     if (labels) {
-        h <- hist(x, plot = FALSE, warn.unused=FALSE, ...)
+        myhist <- hist(x, plot = FALSE, warn.unused=FALSE, breaks=breaks, ...)
         if (type == "count") {
-            aa <- max(h$counts) * 0.01
-            grid.text(label = as.character(round(h$counts, 3)), 
-                x = h$mids, y = aa + h$counts, just = c("centre", 
+            aa <- max(myhist$counts) * 0.02
+            grid.text(label = as.character(round(myhist$counts, 3)), 
+                x = myhist$mids, y = aa + myhist$counts, just = c("centre", 
                   "bottom"), default.units = "native")
         }
         else if (type == "percent") {
-            ss <- sum(h$counts)
-            aa <- max(0.01 * h$counts/ss)
-#            cat(h$counts)
-#            cat("\n")
-#            cat(h$counts/ss)
-#            cat("\n")
-#            cat(ss)
-#            cat("\n")
-#            print(h)
-#            cat("\n")
-            grid.text(label = as.character(round(h$counts/ss, 
-                3)), x = h$mids, y = aa + (h$counts/ss), just = c("centre", 
+            sumCounts <- sum(myhist$counts)
+            aa <- max(myhist$counts/sumCounts) * 0.04
+            grid.text(label = as.character(round(100*myhist$counts/sumCounts, 
+                1)), x = myhist$mids, y = aa + (100*myhist$counts/sumCounts), just = c("centre", 
                 "bottom"), default.units = "native")
         }
         else {
-            aa <- max(h$density) * 0.01
-            grid.text(label = as.character(round(h$density, 3)), 
-                x = h$mids, y = aa + h$density, just = c("centre", 
+            aa <- max(myhist$density) * 0.02
+            grid.text(label = as.character(round(myhist$density, 3)), 
+                x = myhist$mids, y = aa + myhist$density, just = c("centre", 
                   "bottom"), default.units = "native")
         }
     }
     if (!is.null(fit)) {
         x = x[!is.na(x)]
-        density = T
+        density <- TRUE
         if (is.null(args)) {
 			if (! require(MASS) ){
 				stop("The MASS package must be loaded to auto-fit distributions.")
@@ -140,43 +177,23 @@ function (x,
                 args = fitdistr(x, fit, start = start)$estimate
             }
         }
-        if (tolower(fit) == "exponential") {
-            dmath = dexp
-        }
-        if (tolower(fit) == "normal") {
-            dmath = dnorm
-        }
-        if (tolower(fit) == "lognormal" | tolower(fit) == "log-normal") {
-            dmath = dlnorm
-        }
-        if (tolower(fit) == "poisson") {
-            dmath = dpois
-        }
-        if (tolower(fit) == "beta") {
-            dmath = dbeta
-        }
-        if (tolower(fit) == "geometric") {
-            dmath = dgeom
-        }
-        if (tolower(fit) == "t") {
-            dmath = dt
-        }
-        if (tolower(fit) == "weibull") {
-            dmath = dweibull
-        }
-        if (tolower(fit) == "cauchy") {
-            dmath = dcauchy
-        }
-        if (tolower(fit) == "gamma") {
-            dmath = dgamma
-        }
-        if (tolower(fit) == "chisq") {
-            dmath = dchisq
-        }
-        if (tolower(fit) == "chi-squared") {
-            dmath = dchisq
-        }
-    }
+
+		dmath = switch( tolower(fit), 
+					   "exponential" = dexp,
+					   "normal"      = dnorm,
+					   "lognormal"   = dlnorm,
+					   "log-normal"  = dlnorm,
+       				   "poisson"     = dpois,
+					   "beta"        = dbeta,
+					   "t"           = dt,
+					   "weibull"     = dweibull,
+					   "cauchy"      = dcauchy,
+					   "gamma"       = dgamma,
+					   "chisq"       = dchisq,
+					   "chi-squared" = dchisq
+					   )
+        } 
+
     if (is.null(args)) {
         args = list(mean = mean(x, na.rm = T), sd = sd(x, na.rm = T))
     }
