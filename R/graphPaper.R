d8c25887422e87feef6a9ff4c114eb5e5b6ffcc9
Daniel Kaplan <dtkaplan@gmail.com>
2012-01-13 09:09:28 -0600

    Documentation on graphpaper.R

--- R/graphPaper.R
+++ R/graphPaper.R
@@ -1,3 +1,29 @@
+#' Traditional graph-paper axes 
+#'
+#' \code{graphPaper} plots out traditional graph-paper axes for making
+#' mathematical plots.  Whereas standard R graphics put the axes at the
+#' edge of the plotting window, \code{graphPaper} puts them through the zeros
+#' and lets you set explicitly the location of tick marks and graph-paper rules.
+#' You can then plot over the axes by using functions such as \code{lines},
+#' \code{points}, or \code{funPlot} (with \code{add=TRUE}).
+#'
+#' @name graphPaper
+#'
+#' @param xticks numerical vector listing the position of x-axis ticks
+#' @param yticks like \code{xticks} but for y-axis
+#' @param xlim as in \code{plot}, sets x-axis limits (otherwise inferred from \code{xticks})
+#' @param ylim similarly, set y-axis limits (otherwise inferred from \code{yticks})
+#' @param xlab as in \code{plot}, sets the y-axis label
+#' @param ylab similarly, set the y-axis label
+#' @param maxxlabels limits the maximum number of tick marks on x-axis
+#' @param maxylabels limits the maximum number of tick marks on y-axis
+#'
+#' @examples
+#' graphPaper(xticks=-5:5,yticks=seq(-2,2,by=.5))
+#' plotFun(sin(x^2)~x,x=range(-4,4),add=TRUE)
+#'
+#' @author Daniel Kaplan (\email{kaplan@@macalester.edu})
+#' @return nothing
 graphPaper = function(xticks=0:5, yticks=-5:5,
   xlim=c(min(xticks),max(xticks)),
   ylim=c(min(yticks),max(yticks)),
