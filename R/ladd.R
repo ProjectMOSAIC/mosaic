6b8a45b91dd82f0c372f2a917cb0f9981d345620
Randall Pruim <rpruim@gmail.com>
2012-01-14 15:05:46 -0500

    augmented ladd() so it now handles multiple panels

--- R/ladd.R
+++ R/ladd.R
@@ -2,9 +2,9 @@
 #' 
 #' Simplified lattice plotting by adding additional elements to existing plots.
 #'
-#' @param x  callable graphical element to be added to a panel in a lattice plot 
-#' @param row row identifying desired panel in multi-panel plots.  
-#' @param col column identifying desired panel in multi-panel plots.  
+#' @param x  callable graphical element to be added to a panel or panels in a lattice plot 
+#' @param col,row identifies desired panel(s) in multi-panel plots.  If missing, all
+#'           columns or rows are used.
 #' 
 #' 
 #' @details
@@ -15,12 +15,15 @@
 #' 
 #' @export
 #' @examples
-#' print(xyplot(rnorm(100) ~rnorm(100)))
+#' p <- xyplot(rnorm(100) ~rnorm(100))
+#' print(p)
 #' ladd(panel.abline(a=0,b=1))
 #' ladd(panel.abline(h=0,col='red'))
 #' ladd(grid.text('Hello'))
 #' ladd(grid.text(x=.95,y=.05,'text here',just=c('right','bottom')))
-#' print(xyplot( rnorm(100) ~ rnorm(100) | factor(rbinom(100,3,.5)), layout=c(2,2) ))
+#' q <- update(p, layout=c(3,2))
+#' ladd(panel.abline(a=0,b=1))
+#' ladd(panel.abline(h=0,col='red'))
 #' ladd( grid.text("(1,1)",gp=gpar(cex=3,alpha=.5)), 1,1)
 #' ladd( grid.text("(2,1)",gp=gpar(cex=3,alpha=.5)), 2,1)
 #' ladd( grid.text("(1,2)",gp=gpar(cex=3,alpha=.5)), 1,2)
@@ -29,6 +32,19 @@
 #' @keywords graphics 
 #' 
 #' 
-ladd <- function(x,col=1,row=1) { 
-	trellis.focus('panel',col,row); x ; trellis.unfocus() 
+ladd <- function (x, col, row) 
+{
+  xUnevaluated <- substitute(x) 
+  layout <- trellis.currentLayout('panel')
+  if (missing(col)) { col <- 1:ncol(layout) }
+  if (missing(row)) { row <- 1:nrow(layout) }
+ 
+  for (r in row) {
+    for (c in col) {
+      cat(paste('Modifying panel (',c,',',r,')...\n',sep=""))
+      trellis.focus("panel", c, r)
+      eval.parent(xUnevaluated, n=2)
+      trellis.unfocus()
+    }
+  }
 }
