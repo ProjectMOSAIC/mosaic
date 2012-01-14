a35dcec6d5ae367ca5da070cbb48427868af2c28
Randall Pruim <rpruim@gmail.com>
2012-01-14 02:31:12 -0500

    moved rpoly2() into rfun.R, so rpoly2.R no longer in collate list

--- R/rfun.R
+++ R/rfun.R
@@ -16,8 +16,8 @@
 #' Things happen in the domain -5 to 5. The function is pretty flat outside of that.
 #' Use \code{seed} to create a fixed function that will be the same for everybody
 #'
-#' @rdname RandomFunctions
-#' @name randomFunctions
+#' @rdname rfun
+#' @name rfun
 #' @keywords random
 #' @aliases rfun rpoly2
 #' @author Daniel Kaplan (\email{kaplan@@macalester.edu})
@@ -31,7 +31,7 @@ rfun <- function(vars=~x&y, seed=NULL, n=0) {
   if( class(vars) != "formula" )
     stop("Must provide a formula, e.g. ~x&y, to identify the variables")
   nmaxes <- ifelse(n==0, ceiling( runif(1,min=4,max=10)), n)
-  varnames = all.vars(vars)
+  varnames <- all.vars(vars)
   nvars <- length(varnames)
   locs <- list()
   for (k in 1:nvars) locs[[k]] <- runif(nmaxes, min=-3,max=3)
@@ -79,7 +79,7 @@ rfun <- function(vars=~x&y, seed=NULL, n=0) {
       for(k in 1:nmaxes){
         foo <- xscales[k]*(x-locs[[1]][k])^2
         for(j in 2:nvars) {
-          x = eval(parse(text=varnames[j]))
+          x <- eval(parse(text=varnames[j]))
           foo <- foo + (x-locs[[j]][k])^2
         }
         res <- res +  signsmax[k]*exp(-foo/9) 
@@ -92,4 +92,24 @@ rfun <- function(vars=~x&y, seed=NULL, n=0) {
   formals(f) <- tmp
   return(f)
 }
-  
+ 
+#' random 2nd degree polynomials
+#'
+#' \code{rpoly2} generates a random 2nd degree poly of 2 vars (as a function)
+#'
+#' @rdname rfun
+#' @inheritParams rfun
+#'
+#' @return a function defined by a 2nd degree polynomial of 2 variables
+#' with coefficients selected randomly according to a Unif(-1,1) distribution.
+#'
+#' @keywords random
+
+rpoly2 <- function(seed=NULL){
+  if( !is.null(seed) ) set.seed(round(seed))
+  coefs <- runif(5,min=-1,max=1)
+  f <- function(x,y){
+    x*coefs[1] + y*coefs[2] + x*y*coefs[3] + x^2*coefs[4] + y^2*coefs[5]
+  }
+  return(f)
+}
