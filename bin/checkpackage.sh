0175ca6209ef233ef3e1e7d9c40e7e529583eb41
Randall Pruim <rpruim@gmail.com>
2012-01-17 00:07:40 -0500

    adjusted to deal with --resave-data:  check now builds first without that flag and build builds with it.  doit all does check (which includes a build), build, install

--- bin/checkpackage.sh
+++ bin/checkpackage.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 export _R_CHECK_FORCE_SUGGESTS_=false
+bin/roxy -p "." --clean
+mv *_*.tar.gz builds/
+R CMD build .
 bin/do2all "R CMD check %p" *_*.tar.gz
--- bin/doitall.sh
+++ bin/doitall.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-# bin/roxy -p "." --clean
-bin/buildpackage.sh
 bin/checkpackage.sh
+bin/buildpackage.sh
 bin/installpackage.sh
