81f2f4b3efc97d483ab87b5ea027ae1633218d36
Nicholas Horton <nicholasjhorton@gmail.com>
2012-03-01 13:17:25 -0500

    Added new helper routines to simplify permutation testing.

---
+++ R/compareMean.R
@@ -0,0 +1,30 @@
+#' Compare means between 2 groups
+#' 
+#' A function to facilitate 2 group permutation tests for a continuous outcome variable
+#' 
+#' @rdname compareMean
+#' @param formula a formula 
+#' @param data a data frame in which \code{x} is evaluated if \code{x} is a
+#' formula.
+#' @param \dots other arguments
+#' @return the difference in means between the second and first group
+#' @author Nicholas Horton (\email{nhorton@@smith.edu})
+#' @seealso \code{\link{do}}, \code{\link{compareProportion}} and \code{\link{shuffle}}
+#' @keywords resampling
+#' @export
+#' @examples
+#' # calculate the observed difference
+#' obs = compareMean(age ~ sex, data=HELPrct); obs
+#' mean(age ~ sex, data=HELPrct)
+#' # calculate the permutation distribution
+#' permute = do(100) * compareMean(age ~ shuffle(sex), 
+#'   data=HELPrct) 
+#' xhistogram(~ result, groups=result>=obs, permute, 
+#'   xlab="difference in means")
+compareMean = function(formula, data=NULL, ...) {
+  means = mean( formula, data=data, ... )
+  if (length(means$S) != 2) {
+  	stop("number of levels for grouping variable must be 2\n")
+  }
+  return(diff(means$S))
+}
---
+++ R/compareProportion.R
@@ -0,0 +1,31 @@
+#' Compare proportions between 2 groups
+#' 
+#' A function to facilitate 2 group permutation tests for a categorical outcome variable
+#' 
+#' @rdname compareProportion
+#' @param formula a formula 
+#' @param data a data frame in which \code{x} is evaluated if \code{x} is a
+#' formula.
+#' @param \dots other arguments
+#' @return the difference in proportions between the second and first group
+#' @author Nicholas Horton (\email{nhorton@@smith.edu})
+#' @seealso \code{\link{do}}, \code{\link{compareMean}} and \code{\link{shuffle}}
+#' @keywords resampling
+#' @export
+#' @examples
+#' # calculate the observed difference
+#' obs = compareProportion(homeless=="housed" ~ sex, data=HELPrct); obs
+#' mean(homeless=="housed" ~ sex, data=HELPrct)
+#' # calculate the permutation distribution
+#' permute = do(100) * compareProportion(homeless=="housed" ~ shuffle(sex), 
+#'   data=HELPrct)
+#' xhistogram(~ result, groups=result>=obs, permute, 
+#'   xlab="difference in proportions")
+compareProportion = function(formula, data=NULL, ...) {
+  means = mean( formula, data=data, ... )
+  if (length(means$S) != 2) {
+  	stop("number of levels for grouping variable must be 2\n")
+  }
+  return(diff(means$S))
+}
+
