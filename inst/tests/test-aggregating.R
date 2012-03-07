06ccfc07d5875ca8c505032e25c49877572f23d1
Randall Pruim <rpruim@gmail.com>
2012-03-07 13:05:41 -0500

    unit tests added for aggregating functions

---
+++ inst/tests/test-aggregating.R
@@ -0,0 +1,23 @@
+
+context('Aggregating Functions')
+
+test_that("formula interface works", {
+  expect_equivalent( count(~sex, data=HELPrct), sum(HELPrct$sex == 'female') )
+  expect_equivalent( prop(~sex, data=HELPrct), sum(HELPrct$sex == 'female') / nrow(HELPrct))
+  expect_equivalent( mean(~cesd, data=HELPrct), mean(HELPrct$cesd))
+  expect_equivalent( var(~cesd, data=HELPrct), var(HELPrct$cesd))
+  expect_equivalent( sd(~cesd, data=HELPrct), sd(HELPrct$cesd))
+  expect_equivalent( max(~cesd, data=HELPrct), max(HELPrct$cesd))
+  expect_equivalent( min(~cesd, data=HELPrct), min(HELPrct$cesd))
+})
+
+test_that("data frame interface works", {
+  expect_equivalent( count(sex, data=HELPrct), sum(HELPrct$sex == 'female') )
+  expect_equivalent( prop(sex, data=HELPrct), sum(HELPrct$sex == 'female') / nrow(HELPrct))
+  expect_equivalent( mean(cesd, data=HELPrct), mean(HELPrct$cesd))
+  expect_equivalent( var(cesd, data=HELPrct), var(HELPrct$cesd))
+  expect_equivalent( sd(cesd, data=HELPrct), sd(HELPrct$cesd))
+  expect_equivalent( max(cesd, data=HELPrct), max(HELPrct$cesd))
+  expect_equivalent( min(cesd, data=HELPrct), min(HELPrct$cesd))
+})
+
