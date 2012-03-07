e73264b8dc0edd2e714cd995df435385852729b3
Daniel Kaplan <dtkaplan@gmail.com>
2012-03-07 09:48:58 -0600

    Just before writing test statements for D() antiD() etc.

--- sandbox/Danny/antiDideas.R
+++ sandbox/Danny/antiDideas.R
@@ -23,7 +23,8 @@ makeAntiDfun <- function(.function, .wrt, from, to, .tol) {
   # Combine default args with those given in the function call
   res <- function() {
     numerical.integration(.function,.wrt, 
-                          as.list(match.call())[-1],formals())
+                          as.list(match.call())[-1],
+                          formals())
   }
   resargs <- formals(.function) 
   resargs[[.wrt]] <- NULL
@@ -35,15 +36,24 @@ makeAntiDfun <- function(.function, .wrt, from, to, .tol) {
 }
 # =============
 numerical.integration <- function(f,wrt,av,args) {
+  # make sure expressions get evaluated
+  for(k in 1:length(av)) {
+    if(inherits(av[[k]],c("call","name"))) 
+      av[[k]] = eval.parent(av[[k]],n=2)
+  }
+  for(k in 1:length(args)) {
+    if(inherits(args[[k]],c("call","name"))) 
+      args[[k]] = eval.parent(args[[k]],n=2)
+  }
   # Extract the limits from the argument list
   av2 = c(av, args) # combine the actual arguments with the formals
+
   # to make sure that default values are included
   vi.from <- inferArgs(wrt, av2, defaults=alist(val=NaN), 
                        variants = c("from",".from"))$val
   vi.to <- inferArgs(wrt, av2, defaults=alist(val=NaN), 
                      variants = c("to",".to"))$val
-  browser()
-  if( is.nan(vi.to) | is.nan(vi.from)) stop("Integration bounds not given.")
+  if( any(is.nan(vi.to)) | any(is.nan(vi.from))) stop("Integration bounds not given.")
   # and delete them from the call
   av[[paste(wrt,".from",sep="")]] <- NULL
   av[[paste(wrt,".to",sep="")]] <- NULL
@@ -51,16 +61,38 @@ numerical.integration <- function(f,wrt,av,args) {
     av[[wrt]] = vi
     do.call(f,av,quote=TRUE) + 0*vi #make the same size as vi
   }
+  integrateFun( newf, vi.from, vi.to)
   # NEED TO ADD TOLERANCE
-  # Copy code from old antiD
-  # But first test it out for scalar inputs.
-  integrate(newf, vi.from, vi.to )$value
+}
+# =======================
+integrateFun = function(f, from, to){
+  # handle the case where to is fixed and from is assigned to multiple values
+  multiplier <- 1
+  if( length(from) > 1 & length(to) == 1 ){
+    temp <- to
+    to <- from
+    from <- temp
+    multiplier <- -1
+  }
+  # handle situation where both from and to are a set of values.
+  if( length(from)>1 & length(to)>1 ){
+    if( length(from)!=length(to) ) stop("Either fix 'from' or set it to the same length as 'to'")
+    res <- rep(0,length(to))
+    for (k in 1:length(to)) {
+      res[k] <- integrate(f,from[k],to[k])$value
+    }
+    return(res)
+  }
+  val0 <- integrate(f, from, to[1])$value
+  if (length(to) == 1) {
+    return(multiplier*val0)
+  }
+  res <- rep(val0, length(to))
+  for (k in 2:length(res)) {
+    res[k] <- integrate(f, to[k - 1], to[k])$value
+  }
+  res <- cumsum(res)
+  return(multiplier*res)
 }
 
 
-arglist = list(x.from=9, x.to=100)
-vi.from = inferArgs( c("x"), arglist, defaults=alist(val=0), 
-           variants = c("from",".from"))$val
-vi.to = inferArgs( c("x"), arglist, defaults=alist(val=NULL), 
-           variants = c("to",".to"))$val
-print( c(vi.from, vi.to))
