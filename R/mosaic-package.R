c926711a68e4ed5fb13dbfdf504c1e371e62d0c5
Nicholas Horton <nicholasjhorton@gmail.com>
2012-03-22 16:06:57 -0400

    Fixed bargraph.R to address using a "Freq" or ".Freq" variable. Fixed documentation in other routines.

--- R/mosaic-package.R
+++ R/mosaic-package.R
@@ -1,7 +1,7 @@
 #' mosaic
 #'
 #' Data sets and utilities from Project MOSAIC (mosaic-web.org) used to teach mathematics, 
-#' statistics, computation and modeling.  Funded by the NSF, Project MOSAIC is creating a 
+#' statistics, computation and modeling.  Funded by the NSF, Project MOSAIC is a 
 #' community of educators working to tie together aspects of quantitative work that students 
 #' in science, technology, engineering and mathematics will need in their professional lives, 
 #' but which are usually taught in isolation, if at all.
@@ -11,7 +11,7 @@
 #' @aliases mosaic mosaic-package
 #' @docType package
 #' @title mosaic: the Project MOSAIC package
-#' @author Randall Pruim (\email{rpruim@@calvin.edu}), Daniel Kaplan (\email{kaplan@@macalester.edu})
+#' @author Randall Pruim (\email{rpruim@@calvin.edu}), Daniel Kaplan (\email{kaplan@@macalester.edu}), Nicholas Horton (\email{nhorton@@smith.edu})
 #' @references
 #' \url{http://mosaic-web.org}
 #'
