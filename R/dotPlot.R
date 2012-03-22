1cafacc64e500a23be8df949a1105f038fe3f7a3
Nicholas Horton <nicholasjhorton@gmail.com>
2012-03-22 19:17:14 -0400

    More documentation changes.

--- R/dotPlot.R
+++ R/dotPlot.R
@@ -12,6 +12,8 @@
 #' @param breaks,equal.widths,groups,pch,col,lty,lwd,col.line,type,alpha  
 #'     as in \code{\link{histogram}} 
 #'
+#' @param cex  a ratio by which to increase or decrease the dot size
+#'
 #' @param \dots  additional arguments 
 #' 
 #' @return a trellis object
@@ -39,7 +41,6 @@ function (x,
     histogram(x, type = "count", panel = panel, nint = nint, breaks = breaks, ...)
 }
 
-#' @param cex  a ratio by which to increase or decrease the dot size
 #' @rdname dotPlot
 
 panel.dotPlot <-
