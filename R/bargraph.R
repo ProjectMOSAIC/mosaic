6432f97c4b241bfea53ce6bd59bf9d33cf046248
Nicholas Horton <nicholasjhorton@gmail.com>
2012-03-22 13:31:22 -0400

    Added functionality to bargraph() to inherit from parent environment as well as address the situation where it is called with a variable named "Freq"

--- R/bargraph.R
+++ R/bargraph.R
@@ -14,20 +14,21 @@
 #'        \code{\link[lattice]{barchart}} set \code{origin} to \code{NULL}, but
 #'         0 is a better default, and if 0 is not good, perhaps you should use
 #'        a different kind of plot anyway.
+#' @param ylab a character of length one used for the y-axis label
 #' @return a trellis object describing the plot
 #' @seealso \code{link[lattice]{barchart}}
 #'
 #' @note
 #' The current implementation may not work if one of the variables used in the plot
-#' is called \code{Freq}, since that variable is created by an underlying call to 
+#' is called \code{.Freq}, since that variable is created by an underlying call to 
 #' \code{\link{xtabs}}.
 #'
 #' @examples
 #' bargraph( ~ substance, data=HELPrct)
 #' bargraph( ~ substance, data=HELPrct, horizontal=TRUE)
-#' bargraph( ~ substance | sex, groups=homeless, data=HELPrct)
+#' bargraph( ~ substance | sex, groups=homeless, auto.key=TRUE, data=HELPrct)
 
-bargraph <- function(x, data, groups, horizontal=FALSE, origin=0, ...) {
+bargraph <- function(x, data=parent.frame(), groups, horizontal=FALSE, origin=0, ylab="Frequency", ...) {
   sgroups <- substitute(groups)
   haveGroups <- !missing(groups)
   formula <- paste("~", deparse(rhs(x)))
@@ -35,21 +36,24 @@ bargraph <- function(x, data, groups, horizontal=FALSE, origin=0, ...) {
   if (haveGroups ) formula <- paste(formula, "+" , sgroups )
   formula <- as.formula(formula)
   xtab <- as.data.frame(xtabs( formula, data=data))
+  # rename the last column to ".Freq", to handle the case that 
+  # there is a variable called "Freq"
+  names(xtab)[ncol(xtab)] <- ".Freq"
   if (horizontal) {
 	  if (! is.null(condition(x))){
-		formula <- as.formula( paste(deparse(rhs(x)), " ~ Freq | ", deparse(condition(x)) ) )
+		formula <- as.formula( paste(deparse(rhs(x)), " ~ .Freq | ", deparse(condition(x)) ) )
 	  } else {
-		formula <- as.formula( paste(deparse(rhs(x)), " ~ Freq") )
+		formula <- as.formula( paste(deparse(rhs(x)), " ~ .Freq") )
 	  }
   } else {
 	  if (! is.null(condition(x))){
-		formula <- as.formula( paste("Freq ~", deparse(rhs(x)), "|", deparse(condition(x)) ) )
+		formula <- as.formula( paste(".Freq ~", deparse(rhs(x)), "|", deparse(condition(x)) ) )
 	  } else {
-		formula <- as.formula( paste("Freq ~", deparse(rhs(x))) )
+		formula <- as.formula( paste(".Freq ~", deparse(rhs(x))) )
 	  }
   }
   if (haveGroups)
-    barchart( formula, data=xtab, groups=eval(sgroups), origin=origin, ... ) 
+    barchart( formula, data=xtab, groups=eval(sgroups), origin=origin, ylab=ylab, ... ) 
   else
-    barchart( formula, data=xtab, origin=origin, ... )   
+    barchart( formula, data=xtab, origin=origin, ylab=ylab, ... )   
 }
