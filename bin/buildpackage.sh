013951a16c067feaa7823eefc2766e0f771f38bb
Randall Pruim <rpruim@gmail.com>
2012-03-25 00:17:56 -0400

    update to 0.4-0; cp vignettes to inst/doc in buildpackage.sh

--- bin/buildpackage.sh
+++ bin/buildpackage.sh
@@ -2,4 +2,6 @@
 export _R_CHECK_FORCE_SUGGESTS_=false
 bin/roxy -p "." --clean
 mv *_*.tar.gz builds/
+cp vignettes/Resampling/Resampling.pdf inst/doc/Resampling-src.pdf
+cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus-src.pdf
 R CMD build --resave-data .
