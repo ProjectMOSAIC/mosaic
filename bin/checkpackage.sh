20e616d00793359f820e475c43a2de7897181f98
Randall Pruim <rpruim@gmail.com>
2012-03-25 00:35:16 -0400

    updates to checkpackage.sh and Resampling-src.pdf

--- bin/checkpackage.sh
+++ bin/checkpackage.sh
@@ -2,5 +2,7 @@
 export _R_CHECK_FORCE_SUGGESTS_=false
 bin/roxy -p "." --clean
 mv *_*.tar.gz builds/
+\cp vignettes/Resampling/Resampling.pdf inst/doc/Resampling-src.pdf
+\cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus-src.pdf
 R CMD build --resave-data .
 bin/do2all "R CMD check %p" *_*.tar.gz
