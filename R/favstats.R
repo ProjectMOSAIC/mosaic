bb2bbe5bc9acfab8dfcd7f35af08631be2436047
Randall Pruim <rpruim@gmail.com>
2012-03-24 22:46:37 -0400

    added favstats,formula-ANY method

--- R/favstats.R
+++ R/favstats.R
@@ -23,6 +23,28 @@ setGeneric(
 
 
 #' @rdname favstats
+#' @aliases favstats,ANY-method
+#' @export
+setMethod(
+		  'favstats',
+		  c(x='any'),
+		  function (x, ..., na.rm = TRUE) 
+		  {
+			  qq <- quantile(x, na.rm = na.rm)
+			  val <- data.frame(qq[1],  qq[2], qq[3], qq[4], qq[5],
+								mean(x, na.rm = na.rm), 
+								sd(x, na.rm = na.rm), 
+								sum(! is.na(x)),
+								sum( is.na(x) )
+								)
+			  rownames(val) <- ""
+			  names(val) <- c("min", "Q1", "median", "Q3", "max", "mean", "sd", "n", "missing")
+			  return(val)
+		  }
+)
+
+
+#' @rdname favstats
 #' @aliases favstats,numeric-method
 #' @export
 setMethod(
