d0e2a03e36e54540c821cc8359c04269215b615a
cbocovic <cecylia.bocovich@gmail.com>
2012-06-14 16:58:11 -0500

    Added panel functionality.  Still need to figure out lattice graphics.

--- sandbox/Cecylia/plotGrad.R
+++ sandbox/Cecylia/plotGrad.R
@@ -5,10 +5,11 @@
 plotGrad <- function(object, ..., add=FALSE,
                     xlim=NULL, ylim=NULL, npts=NULL,
                     ylab=NULL, xlab=NULL, groups=NULL,
+                    col = FALSE,
                     alpha=NULL ){
   
   if (add) { 
-    ladd( panel.plotFun( object, npts=npts, alpha=alpha, ...))
+    ladd( panel.plotGrad( object, npts=npts, alpha=alpha, ...))
     return(invisible(NULL))
   }
   
@@ -73,20 +74,81 @@ plotGrad <- function(object, ..., add=FALSE,
   #ensure size of arrowhead does not overpower arrow.
   arrowlength = max(diff(limits$xlim)/(50), mean(grid$dx,grid$dy)/2)
   
-  #myplot = lattice::xyplot(grid$Var1, grid$Var2, xlab=xlab, ylab=ylab,xlim=c(limits$xlim),ylim=c(limits$ylim))
+  #Add optional color to gradient vectors specifying length
+  if(col==TRUE){
+    colors = topo.colors(4)
+    colvec = sqrt(grid$dx^2+grid$dy^2)#find and normalize length
+    colvec = colvec/max(colvec)*4   #of vectors
+    colvec = ceiling(colvec)
+  }
+  else colvec=par("fg")#default
+  
+  y=rep(0,0)
+  x=rep(0,0)
+  #myplot = lattice::xyplot(y~x, xlab=xlab, ylab=ylab,xlim=c(limits$xlim),ylim=c(limits$ylim)) #creates a black lattice plot
   myplot=plot(0, cex=0, xlab=xlab, ylab=ylab,xlim=c(limits$xlim),ylim=c(limits$ylim))#need to change this to something else
-  suppressWarnings(arrows(grid$Var1, grid$Var2,grid$Var1+grid$dx,grid$Var2+grid$dy, length = arrowlength,lwd=1.5))
+  suppressWarnings(arrows(grid$Var1, grid$Var2,grid$Var1+grid$dx,grid$Var2+grid$dy, col=colvec, length = arrowlength,lwd=1.5))
   return(myplot)
 }
 
-#Still need to add
+#'Overlays arrows over another function
+#'
+#'
 panel.plotGrad <- function(object, ..., add=FALSE,
                            xlim=NULL, ylim=NULL, npts=NULL,
                            ylab=NULL, xlab=NULL, groups=NULL,
                            alpha=NULL ){
   
   dots <- list(...)
+  dots[['alpha']] <- alpha #Not quite sure what alpha does yet.
+  
+  ..f.. <- do.call( "makeFun", c(object, dots, strict.declaration=FALSE), envir= parent.frame())  
+  
+  vars <- formals(..f..)
+  rhsVars <- all.vars(rhs(object))
+  otherVars <- setdiff(names(vars), rhsVars)
+  ndims <- length(rhsVars)
+  cleanDots <- dots
+  for (v in otherVars) {
+    cleanDots[[v]] <- NULL
+  }
   
   parent.xlim <- current.panel.limits()$xlim
   parent.ylim <- current.panel.limits()$ylim
+  
+  npts <- ifelse( is.null(npts), 10, npts)
+  
+  .xvals <- seq(min(parent.xlim),max(parent.xlim),length=npts)
+  .yvals <- seq(min(parent.ylim),max(parent.ylim),length=npts)
+  stepSize <- diff(parent.xlim)/npts
+  
+  obj = object
+  obj[[3]]<- parse(text=rhsVars[1])[[1]] #replace rhs with on var
+  partial1 = D(obj)#with respect to first var
+  obj[[3]]<- parse(text=rhsVars[2])[[1]] #replace rhs with other var
+  partial2 = D(obj)#with respect to second var
+  
+  deltax = outer(.xvals, .yvals, function(x,y){partial1(x,y)})
+  deltay = outer(.xvals, .yvals, function(x,y){partial2(x,y)})
+  grid <- expand.grid(.xvals,.yvals)
+  grid$dx = c(deltax)
+  grid$dy = c(deltay)
+  
+  #rescale derivatives - we want the maximum length of an arrow to be the distance to the nearest arrow
+  maxd = max(abs(grid$dx), abs(grid$dy))
+  grid$dx = (grid$dx/maxd)*(diff(parent.xlim)/npts)
+  grid$dy = (grid$dy/maxd)*(diff(parent.ylim)/npts)
+  
+  #This does not work here...
+  #Widen limits to fit in arrows
+  #limits$xlim[1] = limits$xlim[1]-min(grid$dx)
+  #limits$xlim[2] = limits$xlim[2]+max(grid$dx)
+  #limits$ylim[1] = limits$ylim[1]-min(grid$dy)
+  #limits$ylim[2] = limits$ylim[2]+max(grid$dy)
+  
+  #ensure size of arrowhead does not overpower arrow.
+  arrowlength = max(diff(parent.xlim)/(50), mean(grid$dx,grid$dy)/2)
+
+  suppressWarnings(panel.arrows(grid$Var1, grid$Var2,grid$Var1+grid$dx,grid$Var2+grid$dy, length = arrowlength,lwd=1.5))
+  
 }
