1f01dd0a33067dc940849010d2e60063a0bcba04
cbocovic <cecylia.bocovich@gmail.com>
2012-07-17 14:55:35 -0500

    Modified .findPoints to work for all systems, fixed a bug in Broyden, findZeros now works for adding equations to a system with more than one equation in it initially.
    Still worried about how users will define formulas

--- R/findZerosMult.R
+++ R/findZerosMult.R
@@ -4,7 +4,8 @@
 #'All free variables (all but the variable on the right side) named in the expression must be assigned 
 #' a value via \code{\ldots}
 #'
-#'@param ... arguments for values 
+#'@param ... arguments for values NOTE: if the system has more than one equation and the rhs
+#'variables do not match up, there will be an error.
 #'@param npts number of desired zeros to return
 #'@param rad radius around center in which to look for zeros
 #'@param center center of search for zeros
@@ -22,7 +23,7 @@
 #' findZerosMult(a*x^2-8~a&x, npts = 50)
 #' findZerosMult(a^2+x^2-8~a&x, npts = 1000, sortBy='radial')
 #'
-findZerosMult <- function(..., npts=10, rad = 5, center=c(0,0), sortBy='byx'){
+findZerosMult <- function(..., npts=10, rad = 5, center=NULL, sortBy='byx'){
   dots = list(...)
   system = list()
   freeVars = list()
@@ -52,48 +53,88 @@ findZerosMult <- function(..., npts=10, rad = 5, center=c(0,0), sortBy='byx'){
   
   lhsOnlyVars = setdiff(allvars, rhsVars)
   
+  if(is.null(center))
+    center = rep(0, length(rhsVars))
   #make sure all equations in the system are functions.
   for(i in (1:numEq)) system[[i]] = try(makeFun(system[[i]]),silent=TRUE) #Fix - sloppy
   
-  #if there is only one equation, call .oneEq
+  #if there is only one equation, use uniroot
   if(length(system)==1){ 
     points = .findPoints(system, freeVars, rhsVars, rad, center, npts)
-    for(i in (1:length(system))){
-      for(j in (1:length(rows(points[[i]])))){
-        pt1= points[[i]][j,]
-        for(k in (1:length(rows(points[[length(points)-i+1]])))){
-          pt2 = points[[length(points)-i+1]][k,]
-          #Find the root
-          newf<-function(t){}
-          #construct the body of the function.
-          newbody = "{"
-          for(l in (1:length(rhsVars))){
-            newbody = paste(newbody,rhsVars[l], "=t*",toString(pt1[l]),
-                            "+(1-t)*",toString(pt2[l]),"\n")
-          }
-          newbody = parse(text = paste(newbody, 
-                                       "do.call(system[[1]],as.list(c(parse(text=rhsVars), freeVars)))}"))
-          body(newf)<- newbody
-          troot = uniroot(newf, c(0,1))$root
-          root=pt1
-          root = troot*pt1+(1-troot)*pt2
-          roots <- rbind(roots, root)
-          if(length(rows(roots)) >= npts){
-            colnames(roots) = rhsVars
-            return(.sort(roots, center = center, type=sortBy)) #return ordered by the first variable.
-          }
+    for(j in (1:length(rows(points[[1]])))){
+      pt1= points[[1]][j,]
+      for(k in (1:length(rows(points[[length(points)]])))){
+        pt2 = points[[2]][k,]
+        #Find the root
+        newf<-function(t){}
+        #construct the body of the function.
+        newbody = "{"
+        for(l in (1:length(rhsVars))){
+          newbody = paste(newbody,rhsVars[l], "=t*",toString(pt1[l]),
+                          "+(1-t)*",toString(pt2[l]),"\n")
+        }
+        newbody = parse(text = paste(newbody, 
+                                     "do.call(system[[1]],as.list(c(parse(text=rhsVars), freeVars)))}"))
+        body(newf)<- newbody
+        troot = uniroot(newf, c(0,1))$root
+        root=pt1
+        root = troot*pt1+(1-troot)*pt2
+        roots <- rbind(roots, root)
+        if(length(rows(roots)) >= npts){
+          colnames(roots) = rhsVars
+          return(.sort(roots, center = center, type=sortBy)) #return ordered by the first variable.
         }
       }
     }
     colnames(roots) = rhsVars
-    return(roots[order(roots[1]),])
+    return(.sort(roots, center = center, type=sortBy))
   }
   
   #Use Broyden when system has more than one equation.
   if(length(system)>1){
+    set.seed(23)
+    
     if(length(system) < length(rhsVars)){ #Need to add equations
-      
+      junk = runif(length(rhsVars), min=-2, max=2)
+      if(1< (length(rhsVars) - length(system)))
+        for(i in 1:(length(rhsVars) - length(system)))
+          junk = cbind(junk, runif(length(rhsVars, min=-2,max=2)))
+      points = .findPoints(system, freeVars, rhsVars, rad, center, npts)
+      for(i in (1:length(system))){
+        for(j in (1:(length(rows(points[[i]]))))){#might be a more efficient way to do this
+          for(k in (1:(length(rows(points[[length(points)-i+1]]))))){
+            browser()
+            pt1 = points[[i]][j,]
+            pt2 = points[[length(points)-i+1]][k,]
+            
+            m = cbind(t(pt2-pt1), junk)
+            norm = qr(m)$qr[,2]
+            hyperplane = function(){}
+            body(hyperplane) <- parse(text=paste("(c(",
+              toString(norm), ")%*%(c(", paste(rhsVars, collapse=","), ")-c(", toString(pt1), ")))[[1]]", sep=""
+              ))
+            formals(hyperplane) <- eval(parse( 
+              text=paste( "as.pairlist(alist(", 
+                          paste(rhsVars, "=", collapse=",", sep=""), "))"
+              )
+            ))
+            environment(hyperplane) <- environment(system[[1]])
+            newsystem = append(system, hyperplane)
+            root = try(Broyden(newsystem, rhsVars, x=center+.001, maxiters=1e3), silent=TRUE)
+            if(!inherits(root, "try-error"))
+              roots = rbind(roots, root)
+            if(length(rows(roots)) >= npts){
+              colnames(roots) = rhsVars
+              return(.sort(roots, center = center, type=sortBy)) #return ordered by the first variable.
+            }
+          }
+        }
+      }
+      colnames(roots) = rhsVars
+      return(.sort(roots, center = center, type=sortBy))
+
     }
+    
     root = Broyden(system, rhsVars, x=center+.001)#often does not work for (0,0)
     roots = rbind(roots, root)
     colnames(roots) = rhsVars
@@ -122,24 +163,24 @@ findZerosMult <- function(..., npts=10, rad = 5, center=c(0,0), sortBy='byx'){
                                                return(angle)})
     data <- data[order(data$angle),]
     
-#     for(i in (1:(npts-2))){
-#      if(sqrt((data[i,1]-data[i+1,1])^2+(data[i,2]-data[i+1,2])^2) >= 
-#        sqrt((data[i,1]-data[i+2,1])^2+(data[i,2]-data[i+2,2])^2)){
-#        browser()
-#        ref.x = mean(data[i,1],data[i+1,1],data[i+2,1])
-#        ref.y = mean(data[i,2],data[i+1,2],data[i+2,2])
-#        ref = c(ref.x,ref.y)
-#         
-#         #find new angles with new reference point
-#        data$angle <- apply(data, 1, function(row){angle <- atan((ref[2]-row[2])/(ref[1]-row[1]))
-#                                                   if(sign(ref[1]-row[1])==-1) angle = angle+pi
-#                                                   if(sign(angle)==-1) angle = 2*pi+angle
-#                                                   return(angle)})
-#         
-#        data[i:npts,]=data[i-1+order(data$angle[i:npts]),]
-#      }
-#     }
-
+    #     for(i in (1:(npts-2))){
+    #      if(sqrt((data[i,1]-data[i+1,1])^2+(data[i,2]-data[i+1,2])^2) >= 
+    #        sqrt((data[i,1]-data[i+2,1])^2+(data[i,2]-data[i+2,2])^2)){
+    #        browser()
+    #        ref.x = mean(data[i,1],data[i+1,1],data[i+2,1])
+    #        ref.y = mean(data[i,2],data[i+1,2],data[i+2,2])
+    #        ref = c(ref.x,ref.y)
+    #         
+    #         #find new angles with new reference point
+    #        data$angle <- apply(data, 1, function(row){angle <- atan((ref[2]-row[2])/(ref[1]-row[1]))
+    #                                                   if(sign(ref[1]-row[1])==-1) angle = angle+pi
+    #                                                   if(sign(angle)==-1) angle = 2*pi+angle
+    #                                                   return(angle)})
+    #         
+    #        data[i:npts,]=data[i-1+order(data$angle[i:npts]),]
+    #      }
+    #     }
+    
     data$angle = NULL
     return(data)
   }
@@ -154,21 +195,20 @@ findZerosMult <- function(..., npts=10, rad = 5, center=c(0,0), sortBy='byx'){
   points = list()
   for(i in (1:numBins)) points[[i]] = data.frame()
   set.seed(1) #set seed for random number generation
-  for(i in (1:100)){
-    point1 =runif(length(vars), min=center[1]-rad, max=center[1]+rad)
-    
-    if(sign(do.call(system[[1]], as.list(c(point1, freeVars))) )==1){#change for more than 2 binds
-      points[[1]] = rbind(points[[1]], point1)
+  for(p in (1:100)){
+    pt =runif(length(vars), min=center[1]-rad, max=center[1]+rad)
+    binNum = 1
+    for(i in (1:(length(system)))){
+      if(!sign(do.call(system[[i]], as.list(c(pt, freeVars))) )==1) #Positive
+        binNum = binNum + 2^(i-1)
     }
-    else{
-      points[[2]] = rbind(points[[2]], point1)
-    }
+    points[[binNum]] = rbind(points[[binNum]], pt)
   }
+  #   if(length(points[[1]])==0||length(points[[2]])==0){
+  #     warning("No zeros found. Try choosing a different start value or widening your search.")
+  #     return(numeric(0))
+  #   }
   
-  if(length(points[[1]])==0||length(points[[2]])==0){
-    warning("No zeros found. Try choosing a different start value or widening your search.")
-    return(numeric(0))
-  }
   for(i in (1:length(points))) colnames(points[[i]]) = vars
   return(points)
 }
@@ -210,7 +250,8 @@ Broyden <- function(system, vars, x=0, tol = .Machine$double.eps^0.5, maxiters=1
   FF=.evalSys(x,system)
   
   for(iter in (1:maxiters)){
-    if(max(abs(FF))<tol) break
+    if(max(abs(FF))<tol) 
+      return(x)
     xnew=as.vector(x-A%*%FF)
     names(xnew)=names(x)
     del = xnew-x
@@ -221,7 +262,16 @@ Broyden <- function(system, vars, x=0, tol = .Machine$double.eps^0.5, maxiters=1
     A=Anew
     FF=FFnew
   }
-  return(x)
+
 }
 
+check_root = function(system, roots){
+  browser()
+  for(i in length(rows(roots))){
+    for(func in system){
+      colnames(roots) = names(formals(func))
+      print(do.call(func, as.list(roots[i,])))
+    }
+  }
+}
 
