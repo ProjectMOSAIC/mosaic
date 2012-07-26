b639077ac63f51ab260ce7827d37a89e0b993b33
Cecylia Bocovich <cbocovic@MATHLAB08-121.local>
2012-07-26 15:05:44 -0500

    .polynomial.expression() is working for positive powers as long as there are no free variables.

--- sandbox/Cecylia/symbolicInt2.R
+++ sandbox/Cecylia/symbolicInt2.R
@@ -18,294 +18,138 @@
 pattern1 <- list(One='cos', Two="affine")
 
 
-for(ptrn in patternList){
+#for(ptrn in patternList){
   
-}
+#}
 
-#Notes to change this: be able to identify 3*x+x as an affine expression as well.
-#This is really important:
-# > .affine.exp(lhs(3*x^2+2~x), 'x^2')
-# $a
-# [1] 3
-# 
-# $b
-# 0 + 2
 
-#'Takes a call and returns its affine coefficients.
-#'
-#'@param tree A call that will be parse
-#'@param .x. the variable name
-#'
-#'@return A list with values of a and b satisfying a*.x.+b  = tree.
-#'If the expression is not affine, returns an empty list.
-#'
-.affine.exp <- function(tree, .x.){
-  #if it is a simple expression
-  if(tree==.x.){
-    a=1
-    b=0
-    return(list(a=a,b=b))
-  }
-  
-  #if there is no variable in the expression
-  if(length(grep(toString(.x.), deparse(tree), fixed=TRUE))==0){
-    a=0
-    b=tree
-    return(list(a=a,b=b))
-  }
-  
-  #if the expression is more complex
-  if(tree[[1]]=='('){
-    return(Recall(tree[[2]], .x.))
-  }
-  
-  if(tree[[1]]=='+'){
-    lexp=Recall(tree[[2]], .x.)
-    rexp=Recall(tree[[3]], .x.)
-    
-    if(lexp$a==0 && length(rexp)!=0){
-      a = rexp$a
-      b = parse(text=paste(deparse(lexp$b), "+", deparse(rexp$b),sep=""))[[1]]
-      return(list(a=a,b=b))
-    }
-    
-    if(rexp$a==0 && length(lexp)!=0){
-      a = lexp$a
-      b = parse(text=paste(deparse(lexp$b), "+", deparse(rexp$b),sep=""))[[1]]
-      return(list(a=a,b=b))
-    }
-  }
-  
-  if(tree[[1]]=='-'){
-    lexp=Recall(tree[[2]], .x.)
-    rexp=Recall(tree[[3]], .x.)
-    
-    if(lexp$a==0 && length(rexp)!=0){
-      a = parse(text=paste("-1*(",deparse(lexp$a), ")",sep=""))[[1]]
-      b = parse(text=paste(deparse(lexp$b), "-(", deparse(rexp$b),")",sep=""))[[1]]
-      return(list(a=a,b=b))
-    }
-    
-    if(rexp$a==0 && length(lexp)!=0){
-      a = lexp$a
-      b = parse(text=paste(deparse(lexp$b), "-(", deparse(rexp$b),")",sep=""))[[1]]
-      return(list(a=a,b=b))
-    }
-  }
-  
-  if(tree[[1]]=='*'){
-    lexp=Recall(tree[[2]], .x.)
-    rexp=Recall(tree[[3]], .x.)
-    
-    if(lexp$a==0 && length(rexp)!=0){
-      a = parse(text=paste("(",deparse(lexp$b), ")*(", deparse(rexp$a),")",sep=""))[[1]]
-      b = parse(text=paste("(",deparse(lexp$b), ")*(", deparse(rexp$b),")",sep=""))[[1]]
-      if(lexp$b==0){
-        a = 0
-        b = 0
-      }
-      if(lexp$b==1){
-        a = rexp$a
-        b = rexp$b
-      }
-      if(rexp$a ==0){
-        a = 0
-      }
-      if(rexp$b == 0){
-        b = 0
-      }
-      if(rexp$a == 1){
-        a = lexp$b
-      }
-      if(rexp$b == 1){
-        b = lexp$b
-      }
-      return(list(a=a,b=b))
-    }
-    
-    if(rexp$a==0 && length(lexp)!=0){
-      a = parse(text=paste("(",deparse(lexp$a), ")*(", deparse(rexp$b),")",sep=""))[[1]]
-      b = parse(text=paste("(",deparse(lexp$b), ")*(", deparse(rexp$b),")",sep=""))[[1]]
-      
-      if(rexp$b==0){
-        a = 0
-        b = 0
-      }
-      if(rexp$b==1){
-        a = lexp$a
-        b = lexp$b
-      }
-      if(lexp$a==0){
-        a = 0
-      }
-      if(lexp$b==0){
-        b = 0
-      }
-      if(lexp$a==1){
-        a = rexp$b
-      }
-      if(lexp$b==1){
-        b = rexp$b
-      }
-      return(list(a=a,b=b))
-    }
-  }
-  
-  if(tree[[1]]=='/'){
-    lexp=Recall(tree[[2]], .x.)
-    rexp=Recall(tree[[3]], .x.)
-    
-    if(rexp$a==0 && length(lexp)!=0){
-      a = parse(text=paste(deparse(lexp$a), "/(", deparse(rexp$b),")",sep=""))[[1]]
-      b = parse(text=paste(deparse(lexp$b), "/(", deparse(rexp$b),")",sep=""))[[1]]
-      
-      if(rexp$b==1){
-        a = lexp$a
-        b = lexp$b
-      }
-      
-      return(list(a = a, b = b))
-    }
-  }
-  return(list())
-}
-
-#'Takes a call and returns its quadratic coefficients
+#'Takes a call and returns its polynomial coefficients
 #'
 #'@param tree A call that will be parse
 #'@param .x. the variable name
 #'
 #'@return A list with values of a, b, c, ... satisfying a*.x.^2+b*.x.+c  = tree. The last value in the list, pow, indicates the highest power
 #'of the expression.
-#'If the expression is not a polynomial, returns an empty list.
+#'If the expression is not a polynomial, returns an empty list or an error.
 #'
 .polynomial.expression <- function(tree, .x.){
-  highestPow = 0
+  
+  
   #if it is a simple expression
   if(tree==.x.){
-    a=1
-    b=0
-    return(list(a=a,b=b, pow=1))
+    coeffs <- c(a=1, b=0)
+    return(list(coeffs= coeffs, pow=1))
   }
   
-  #if there is no variable in the expression
-  if(length(grep(toString(.x.), deparse(tree), fixed=TRUE))==0){
-    a=tree
-    return(list(a=a,pow=0))
+  #if it is a constant
+  if(class(tree)=='numeric'||class(tree)=='name'){
+    coeffs <- c(a=tree)
+    return(list(coeffs = coeffs, pow=0))
   }
-  
-  #if the expression is more complex
+
   if(tree[[1]]=='('){
     return(Recall(tree[[2]], .x.))
   }
   
-  if(tree[[1]]=='+'){
-    lexp=Recall(tree[[2]], .x.)
-    rexp=Recall(tree[[3]], .x.)
+  if(tree[[1]] == '+'){
+    lside <- Recall(tree[[2]], .x.)
+    rside <- Recall(tree[[3]], .x.)
     
-    if(lexp$a==0 && length(rexp)!=0){
-      a = rexp$a
-      b = parse(text=paste(deparse(lexp$b), "+", deparse(rexp$b),sep=""))[[1]]
-      return(list(a=a,b=b))
+    if(rside$pow >= lside$pow){
+      pow = rside$pow
+      coeffs <- rside$coeffs
+      lcoeffs <- append(rep(0, pow-lside$pow), lside$coeffs)
+      browser()
+      coeffs <- coeffs + lcoeffs
+      
+      return(list(coeffs = coeffs, pow = pow))
     }
     
-    if(rexp$a==0 && length(lexp)!=0){
-      a = lexp$a
-      b = parse(text=paste(deparse(lexp$b), "+", deparse(rexp$b),sep=""))[[1]]
-      return(list(a=a,b=b))
+    else{
+      pow = lside$pow
+      coeffs <- lside$coeffs
+      rcoeffs <- append(rep(0, pow-rside$pow), rside$coeffs)
+      coeffs <- coeffs + rcoeffs
+      
+      return(list(coeffs = coeffs, pow = pow))
     }
   }
   
-  if(tree[[1]]=='-'){
-    lexp=Recall(tree[[2]], .x.)
-    rexp=Recall(tree[[3]], .x.)
+  if(tree[[1]] == '-'){
+    lside <- Recall(tree[[2]], .x.)
+    rside <- Recall(tree[[3]], .x.)
     
-    if(lexp$a==0 && length(rexp)!=0){
-      a = parse(text=paste("-1*(",deparse(lexp$a), ")",sep=""))[[1]]
-      b = parse(text=paste(deparse(lexp$b), "-(", deparse(rexp$b),")",sep=""))[[1]]
-      return(list(a=a,b=b))
+    if(rside$pow >= lside$pow){
+      pow = rside$pow
+      coeffs <- rside$coeffs
+      lcoeffs <- append(rep(0, pow-lside$pow), lside$coeffs)
+      names <- names(coeffs)
+      coeffs <- lcoeffs - coeffs
+      names(coeffs) <- names
+      
+      return(list(coeffs = coeffs, pow = pow))
     }
     
-    if(rexp$a==0 && length(lexp)!=0){
-      a = lexp$a
-      b = parse(text=paste(deparse(lexp$b), "-(", deparse(rexp$b),")",sep=""))[[1]]
-      return(list(a=a,b=b))
+    else{
+      pow = lside$pow
+      coeffs <- lside$coeffs
+      rcoeffs <- append(rep(0, pow-rside$pow), rside$coeffs)
+      coeffs <- coeffs - rcoeffs
+      
+      return(list(coeffs = coeffs, pow = pow))
     }
   }
   
-  if(tree[[1]]=='*'){
-    lexp=Recall(tree[[2]], .x.)
-    rexp=Recall(tree[[3]], .x.)
+  if(tree[[1]] == '*'){
     
-    if(lexp$a==0 && length(rexp)!=0){
-      a = parse(text=paste("(",deparse(lexp$b), ")*(", deparse(rexp$a),")",sep=""))[[1]]
-      b = parse(text=paste("(",deparse(lexp$b), ")*(", deparse(rexp$b),")",sep=""))[[1]]
-      if(lexp$b==0){
-        a = 0
-        b = 0
-      }
-      if(lexp$b==1){
-        a = rexp$a
-        b = rexp$b
-      }
-      if(rexp$a ==0){
-        a = 0
-      }
-      if(rexp$b == 0){
-        b = 0
-      }
-      if(rexp$a == 1){
-        a = lexp$b
-      }
-      if(rexp$b == 1){
-        b = lexp$b
-      }
-      return(list(a=a,b=b))
+    lside <- Recall(tree[[2]], .x.)
+    rside <- Recall(tree[[3]], .x.)
+    
+    pow <- lside$pow + rside$pow
+    diff <- abs(lside$pow - rside$pow)
+    dim = max(lside$pow, rside$pow)+1
+    
+    cmatrix <- outer(lside$coeffs, rside$coeffs)
+    #pad matrix to make it square
+    if(nrow(cmatrix) > ncol(cmatrix)){
+      for(i in (1:diff))
+        cmatrix <- cbind(cmatrix, 0)
+    }
+    if(ncol(cmatrix) > nrow(cmatrix)){
+      for(i in (1:diff))
+        cmatrix <- rbind(cmatrix, 0)
     }
     
-    if(rexp$a==0 && length(lexp)!=0){
-      a = parse(text=paste("(",deparse(lexp$a), ")*(", deparse(rexp$b),")",sep=""))[[1]]
-      b = parse(text=paste("(",deparse(lexp$b), ")*(", deparse(rexp$b),")",sep=""))[[1]]
-      
-      if(rexp$b==0){
-        a = 0
-        b = 0
+    coeffs <- rep(0, pow+1)
+    for(i in (1:dim)){
+      if(i != 0){
+        coeffs[i] <- sum(cmatrix[cbind((i:1), (1:i))])
       }
-      if(rexp$b==1){
-        a = lexp$a
-        b = lexp$b
-      }
-      if(lexp$a==0){
-        a = 0
-      }
-      if(lexp$b==0){
-        b = 0
-      }
-      if(lexp$a==1){
-        a = rexp$b
-      }
-      if(lexp$b==1){
-        b = rexp$b
-      }
-      return(list(a=a,b=b))
     }
+    for(i in dim:(pow+1)){
+      if(i != 0){
+        coeffs[i] <- sum(cmatrix[cbind((dim:(1+i-dim)), ((1+i-dim):dim))])
+      }
+    }
+    
+    names(coeffs) <- letters[(1:length(coeffs))]
+    
+    return(list(coeffs = coeffs, pow = pow))
   }
   
-  if(tree[[1]]=='/'){
-    lexp=Recall(tree[[2]], .x.)
-    rexp=Recall(tree[[3]], .x.)
+  if(tree[[1]] == '^'){    
+    #Recursively call as multiplication
+    newTree <- tree
     
-    if(rexp$a==0 && length(lexp)!=0){
-      a = parse(text=paste(deparse(lexp$a), "/(", deparse(rexp$b),")",sep=""))[[1]]
-      b = parse(text=paste(deparse(lexp$b), "/(", deparse(rexp$b),")",sep=""))[[1]]
-      
-      if(rexp$b==1){
-        a = lexp$a
-        b = lexp$b
-      }
-      
-      return(list(a = a, b = b))
+    tree[[3]] <- tree[[3]] - 1
+    if(tree[[3]] == 1){
+      tree <- tree[[2]]
     }
+    
+    newTree <- parse(text= paste(deparse(newTree[[2]]), "*", deparse(tree), sep=""))[[1]]
+    return(Recall(newTree, .x.))
+    
   }
+  
   return(list())
 }
