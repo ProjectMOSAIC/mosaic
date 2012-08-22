beab0805203a954b1717dce3b2c065c328bf36db
Randall Pruim <rpruim@gmail.com>
2012-08-22 16:39:49 -0400

    Fix for issue #133 -- TukeyHSD()

--- R/Tukey.R
+++ R/Tukey.R
@@ -1,23 +1,26 @@
-#' @rdname TukeyHSD
 #' Additional interfaces to TukeyHSD
 #' 
 #' TukeyHSD requires use of \code{\link{aov}}.  Since this is a hinderence for beginners, wrappers
 #' have been provided to remove this need.
+#' @rdname TukeyHSD
 #' @param x an object, for example of class \code{lm} or \code{formula}
+#' @param data a data frame.  NB: This does not come second in the argument list.
 #' @param which,ordered,conf.level,\dots just as in \code{\link{TukeyHSD}} from the \code{base} package
 #' 
-#' @method TukeyHSD,lm
+#' @method TukeyHSD lm
+#' @export
 TukeyHSD.lm <- function(x, which, ordered = FALSE, conf.level=0.95, ...) {
-  TukeyHSD.aov( aov(x), which =which, ordered = ordered, conf.level=conf.level, ...)
+  TukeyHSD.aov( aov(x), which = which, ordered = ordered, conf.level = conf.level, ...)
 }
 
 #' @rdname TukeyHSD
-#' @method TukeyHSD,formula
-TukeyHSD.formula <- function(x, data=parent.frame(), which, ordered = FALSE, conf.level=0.95, ...) {
+#' @method TukeyHSD formula
+#' @export
+TukeyHSD.formula <- function(x, which, ordered = FALSE, conf.level=0.95, data=parent.frame(), ...) {
   TukeyHSD.lm( lm(x, data=data, ...), which = which, ordered = ordered, conf.level=conf.level, ...)
 }
 
-#' @export
+
 #' @examples
 #' ## These should all give the same results
 #' model <- lm(age ~ substance, HELPrct)
