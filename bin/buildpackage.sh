050a3da22866669a3583ba4ffb52bfef453deeb6
Randall Pruim <rpruim@gmail.com>
2012-09-03 09:36:54 -0400

    updates to build scripts [MinimalR.pdf]

--- bin/buildpackage.sh
+++ bin/buildpackage.sh
@@ -4,4 +4,5 @@ bin/roxy -p "." --clean
 mv *_*.tar.gz builds/
 cp vignettes/Resampling/Resampling.pdf inst/doc/Resampling-src.pdf
 cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus-src.pdf
+cp vignettes/MinimalR/MinimalR.pdf inst/doc/MinimalR-src.pdf
 R CMD build --resave-data .
--- bin/checkpackage.sh
+++ bin/checkpackage.sh
@@ -4,5 +4,6 @@ bin/roxy -p "." --clean
 mv *_*.tar.gz builds/
 \cp vignettes/Resampling/Resampling.pdf inst/doc/Resampling-src.pdf
 \cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus-src.pdf
+\cp vignettes/MinimalR/MinimalR.pdf inst/doc/MinimalR-src.pdf
 R CMD build --resave-data .
 bin/do2all "R CMD check --as-cran %p" *_*.tar.gz
