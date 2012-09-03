cc90d6932dbff0bcee124d35bf0e42af7f208daa
Randall Pruim <rpruim@gmail.com>
2012-09-03 10:20:08 -0400

    Trying new vignette system
    As per http://cran.r-project.org/web/packages/R.rsp/vignettes/NonSweaveVignettes.pdf

--- bin/buildpackage.sh
+++ bin/buildpackage.sh
@@ -2,7 +2,7 @@
 export _R_CHECK_FORCE_SUGGESTS_=false
 bin/roxy -p "." --clean
 mv *_*.tar.gz builds/
-\cp vignettes/Resampling/Resampling.pdf inst/doc/Resampling-src.pdf
-\cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus-src.pdf
-\cp vignettes/MinimalR/MinimalR.pdf inst/doc/MinimalR-src.pdf
+\cp vignettes/Resampling/Resampling.pdf inst/doc/Resampling.pdf
+\cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus.pdf
+\cp vignettes/MinimalR/MinimalR.pdf inst/doc/MinimalR.pdf
 R CMD build --resave-data .
