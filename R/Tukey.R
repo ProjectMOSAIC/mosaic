43a1b3ae15a793464aba600ca38cdce8b204c530
Randall Pruim <rpruim@gmail.com>
2013-06-20 16:22:20 -0400

    change file name from TukeyHSD to TukeyHSD.lm

--- R/Tukey.R
+++ R/Tukey.R
@@ -3,7 +3,7 @@
 #' \code{\link{TukeyHSD}} requires use of \code{\link{aov}}.  
 #' Since this is a hinderence for beginners, wrappers
 #' have been provided to remove this need.
-#' @rdname TukeyHSD
+#' @rdname TukeyHSD.lm
 #' @param x an object, for example of class \code{lm} or \code{formula}
 #' @param data a data frame.  NB: This does not come second in the argument list.
 #' @param which,ordered,conf.level,\dots just as in \code{\link{TukeyHSD}} from the \code{base} package
@@ -14,7 +14,7 @@ TukeyHSD.lm <- function(x, which, ordered = FALSE, conf.level=0.95, ...) {
 	stats::TukeyHSD.aov( aov(x), which = which, ordered = ordered, conf.level = conf.level, ...)
 }
 
-#' @rdname TukeyHSD
+#' @rdname TukeyHSD.lm
 #' @method TukeyHSD formula
 #' @export
 #' @examples
