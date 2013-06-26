5f14450ef74bdec292f9164e8d9f2307369d1cbc
Randall Pruim <rpruim@gmail.com>
2013-06-25 23:08:20 -0400

    fancier example in sp2df()

--- R/maps.R
+++ R/maps.R
@@ -2,7 +2,7 @@
 #'
 #'Some utilities for working with map data
 #'
-#' @param map a map object of class code{SpatialPolygonsDataFrame}
+#' @param map a map object of class \code{SpatialPolygonsDataFrame}
 #' @param ... other arguments, currently ignored
 #' @export
 #' @examples
@@ -12,24 +12,22 @@
 #'   worldmap <- sp2df(wrld_simpl)
 #' }
 #' 
-sp2df <- function(map,...) {
-  .try_require("ggplot2")   # needed for fortify()
-  
+#' if ( require(ggplot2) && require(maptools) && require(plyr) ) { 
+#'   data(wrld_simpl)
+#'   World <- sp2df(wrld_simpl)
+#'   World2 <- merge(World, Countries, by.x="NAME", by.y="maptools", all.y=FALSE)
+#'   Mdata <- merge(Alcohol, World2, by.x="country", by.y="gapminder", all.y=FALSE) 
+#'   Mdata <- Mdata[order(Mdata$order),]
+#'   qplot( x=long, y=lat, fill=ntiles(alcohol,5), data=subset(Mdata, year==2008), group = group, geom="polygon")
+#' }
+sp2df <- function (map, ...) 
+{
+  .try_require("ggplot2")
   map@data$id <- rownames(map@data)
-  
   coords_matrix <- coordinates(map)
-  map@data$clon = coords_matrix[,1] 
-  map@data$clat = coords_matrix[,2]
-  
-  map_points <- fortify( map, region="id" )
-  result <- merge(map_points, map@data, by="id")
+  map@data$clon = coords_matrix[, 1]
+  map@data$clat = coords_matrix[, 2]
+  map_points <- fortify(map, region = "id")
+  result <- merge(map_points, map@data, by = "id")
   return(result)
 }
-
-# worldmap <- readOGR(dsn="/Users/rpruim/Downloads/TM_WORLD_BORDERS_SIMPL-0.2", 
-#                    layer="TM_WORLD_BORDERS_SIMPL-0.2")
-#
-# world.df <- sp2df(worldmap)
-# world.df2 <- sp2df(wrld_simpl)
-#
-
