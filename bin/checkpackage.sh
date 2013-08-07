8a0318285e70c6b46f75fbadac9253fb7035853e
Randall Pruim <rpruim@gmail.com>
2013-08-06 23:17:57 -0400

    moving vignettes to newly required location

--- bin/checkpackage.sh
+++ bin/checkpackage.sh
@@ -2,8 +2,8 @@
 export _R_CHECK_FORCE_SUGGESTS_=false
 bin/roxy -p "." --clean
 mv *_*.tar.gz builds/
-\cp vignettes/Resampling/Resampling.pdf inst/doc/Resampling.pdf
-\cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus.pdf
-\cp vignettes/MinimalR/MinimalR.pdf inst/doc/MinimalR.pdf
+\cp vignettes/Resampling/Resampling.pdf inst/doc/vignettes/Resampling.pdf
+\cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/vignettes/Calculus.pdf
+\cp vignettes/MinimalR/MinimalR.pdf inst/doc/vignettes/MinimalR.pdf
 R CMD build --resave-data .
 bin/do2all "R CMD check --as-cran %p" *_*.tar.gz
