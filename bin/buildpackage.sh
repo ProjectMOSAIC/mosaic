ed4b2e0b944f734fe81b8fc406f844ba2f7fe7ee
Randall Pruim <rpruim@gmail.com>
2013-08-17 14:24:03 -0400

    updated paths for vignettes

--- bin/buildpackage.sh
+++ bin/buildpackage.sh
@@ -2,7 +2,8 @@
 export _R_CHECK_FORCE_SUGGESTS_=false
 bin/roxy -p "." --clean
 mv *_*.tar.gz builds/
-\cp vignettes/Resampling/Resampling.pdf inst/doc/vignettes/Resampling.pdf
-\cp vignettes/Calculus/mosaic-calculus.pdf inst/doc/vignettes/Calculus.pdf
-\cp vignettes/MinimalR/MinimalR.pdf inst/doc/vignettes/MinimalR.pdf
+\cp vignettes-source/Resampling/Resampling.pdf vignettes/Resampling.pdf
+\cp vignettes-source/Calculus/mosaic-calculus.pdf vignettes/Calculus.pdf
+\cp vignettes-source/MinimalR/MinimalR.pdf vignettes/MinimalR.pdf
+\cp vignettes-source/Links/Links.pdf vignettes/Links.pdf
 R CMD build --resave-data .
