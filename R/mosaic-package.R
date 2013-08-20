31ac4116919e2e8a92301ba846f5c4464ed2405a
Randall Pruim <rpruim@gmail.com>
2013-08-20 16:00:33 -0400

    updated imports statements a la R 3.1 checking

--- R/mosaic-package.R
+++ R/mosaic-package.R
@@ -18,5 +18,11 @@
 #' @keywords package
 #' @importFrom utils head modifyList
 #' @importFrom stats TukeyHSD nls
+#' @import Hmisc   
+#' @import grid    
+#' @import lattice
+#' @import splines 
+#' @import Hmisc
+#' @import methods
 #' @exportPattern "^[^\\.]"
 NA
