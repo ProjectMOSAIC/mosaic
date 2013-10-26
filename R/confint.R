40cfdf41336fb76d23f495d85c1f5226e065cfd0
Randall Pruim <rpruim@gmail.com>
2013-10-26 11:18:10 -0400

    different output format for confint.do.data.frame(); multiple options at once

--- R/confint.R
+++ R/confint.R
@@ -30,42 +30,66 @@
 #' s2 <- do(500)*mean( resample(1:10) ) 
 #' confint(s2)
 # ==================
-confint.numeric = function(object, parm, level=0.95, ..., method=c("stderr", "quantile"),margin=FALSE) {
-  vals = .mosaic.get.ci( object, level, method[1] )
-  if( margin ) return( c(center=mean(vals), margin.of.error=diff(vals)/2) )
-  else return(vals)
+confint.numeric = function(object, parm, level=0.95, ..., method=c("stderr", "quantile"), margin=FALSE) {
+  method <- match.arg(method, c("stderr","percentile","quantile"), several.ok=TRUE)
+  result <- list()
+  for (m in method) {
+    vals <- .mosaic.get.ci( object, level, m )
+    result[[m]] <-  if( margin )  
+      c(center=mean(vals), margin.of.error=diff(vals)/2, method=m, level=level)  else 
+        vals
+  }
+  
+  
 }
 # =================
 #' @rdname confint
 #' @method confint do.data.frame
-confint.do.data.frame = function(object, parm, level=0.95, ..., method=c("stderr", "quantile"), margin=FALSE) {
-  method <- match.arg(method) # which method was selected
-  nms <- names(object)
+confint.do.data.frame = function(object, parm, level=0.95, ..., 
+                                 method=c("stderr", "quantile"), margin=FALSE) {
+  method <- match.arg(method, c("se","stderr","percentile","quantile"), several.ok=TRUE) # which method was selected
+  method[method=="percentile"] <- "quantile"
+  method[method=='se'] <- 'stderr'
+  method <- unique(method)
+  
+  if (missing(parm)) parm <- names(object)
+  nms <- intersect(names(object),parm)
   n <- length(nms)
-  res <- data.frame( name=rep(NA,n), lower=rep(NA,n), upper=rep(NA,n) )
+  res <- data.frame( matrix( nrow=0, ncol=5) )
+  names(res) <- c("name", "lower","upper","level","method")
+  row <- 0
   for (k in 1:n ) {
-    if (is.numeric( object[[nms[k]]] )) {
-      vals <- .mosaic.get.ci( object[[nms[k]]], level, method)
-      res$name[k] <- nms[k]
-      res$lower[k] <- vals[1]
-      res$upper[k] <- vals[2]
+    for (m in method) {
+      for (l in level) {
+        if (is.numeric( object[[nms[k]]] )) {
+          row <- row + 1
+          vals <- .mosaic.get.ci( object[[nms[k]]], l, m)
+          res[row,"name"] <- nms[k]
+          res[row,"lower"] <- vals[1]
+          res[row,"upper"] <- vals[2]
+          res[row,"level"] <- l
+          res[row,"method"] <- m
+          res[row,"point"] <- if(m == "stderr") mean(vals) else mean(object[[nms[k]]], na.rm=TRUE)
+        }
+      }
     }
   }
-  res <- subset(res, !is.na(res$name) ) # get rid of non-quantitative variables
+#  res <- subset(res, !is.na(res$name) ) # get rid of non-quantitative variables
   if( margin ) {
-    res <- data.frame(name=res$name, 
-                     point=(res$upper+res$lower)/2, 
-                     margin.of.error=(res$upper-res$lower)/2)
+#     res[, "point"] <- with( res, (upper+lower)/2 )
+    res[, "margin.of.error"] <- with( res,  (res$upper-res$lower)/2 )
+    res[ res$method!="stderr", "point"] <- NA
+    res[ res$method!="stderr", "margin.of.error"] <- NA
   }
 
   # Change the names to those given by confint.default
-  colnames(res) <- 
-    if (method=="quantile") 
-      c("name", paste(c((1-level)/2, 1-(1-level)/2)*100, "%" ))
-    else c("name", "lower", "upper")
+#  colnames(res) <- 
+#    if (method=="quantile") 
+#      c("name", paste(c((1-level)/2, 1-(1-level)/2)*100, "%" ))
+#    else c("name", "lower", "upper")
   
-  if (margin)  # Report as a center and margin of error
-    res = .turn.to.margin(res)
+#  if (margin)  # Report as a center and margin of error
+#    res = .turn.to.margin(res)
 
   return( res )
 }
@@ -75,11 +99,12 @@ confint.do.data.frame = function(object, parm, level=0.95, ..., method=c("stderr
               margin.of.error=(res[[3]]-res[[2]])/2)
 }
 .mosaic.get.ci = function( vals, level, method ) {
+  alpha <- (1-level)/2
   if( method == "stderr" ) res = mean(vals, na.rm=TRUE) + 
     c(-1,1)*sd(vals, na.rm=TRUE)*
-    qt(1-(1-level)/2, sum(!is.na(vals))-1 )
+    qt(1-alpha, sum(!is.na(vals))-1 )
   # the sum(!is.na(vals)) above is to account for NAs in finding the degrees of freedom
-  else res = qdata( c((1-level)/2, 1-(1-level)/2), vals )
+  else res = qdata( c(alpha, 1-alpha), vals )
   return(res)
 }
 
