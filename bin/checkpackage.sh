4a264052e8dacd0e68ef01e3c75c698568b8bb2a
Randall Pruim <rpruim@gmail.com>
2014-01-14 02:59:34 -0500

    changing from R-devel to R for checkpackage.sh

--- bin/checkpackage.sh
+++ bin/checkpackage.sh
@@ -6,5 +6,5 @@ mv *_*.tar.gz builds/
 #\cp sandbox/Vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus.pdf
 #\cp sandbox/Vignettes/MinimalR/MinimalR.pdf inst/doc/MinimalR.pdf
 #\cp sandbox/Vignettes/Links/Links.pdf inst/doc/Links.pdf
-R-devel CMD build --resave-data .
+R CMD build --resave-data .
 bin/do2all "R-devel CMD check --as-cran %p" *_*.tar.gz
