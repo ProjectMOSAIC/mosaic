f5392809c68d0119ea9d14de7ce2727548a21570
Randall Pruim <rpruim@gmail.com>
2014-02-19 11:20:43 -0500

    add documentation for parallel arg

--- R/do.R
+++ R/do.R
@@ -23,6 +23,8 @@ tryCatch(utils::globalVariables(c('.row')),
 #' @param algorithm a number usd to select the algorithm used.  Currently numbers below 1 
 #' use an older algorithm and numbers >=1 use a newer algorithm which is faster in some 
 #' situations.
+#' @param parallel a logical indicating whether parallel computation should be attempted
+#' using the \pkg{parallel} package (if it is installed).
 #' 
 #' @param e1 an object (in cases documented here, the result of running \code{do})
 #' @param e2 an object (in cases documented here, an expression to be repeated)
