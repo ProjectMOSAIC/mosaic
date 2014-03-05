722c96642c70718dac42f8f4841f7eb68268c9bd
Randall Pruim <rpruim@gmail.com>
2014-03-04 21:00:18 -0500

    adding diffmean() prototype

---
+++ R/diffmean.R
@@ -0,0 +1,19 @@
+#' Difference in means
+#' 
+#' A wrapper around \code{diff(mean(...))} that facilitates better naming of the result
+#' 
+#' @param ... arguments passed to \code{\mean}
+#' @export
+#' @examples
+#' diffmean( age ~ substance, data=HELPrct)
+#' do(3) * diffmean(age ~ substance, data=HELPrct)
+#' diffmean( age ~ sex, data=HELPrct)
+#' do(3) * diffmean(age ~ sex, data=HELPrct)
+ 
+diffmean <- function( ... ) {
+  m <- mean(...)
+  nms <- names(m)
+  res <- diff(m)
+  names(res) <- if (length(nms) < 3) "diffmean" else paste(tail(nms,-1), head(nms, -1), sep="-")
+  res
+}
