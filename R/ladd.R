c4f2f47e04fd74532580c3df703c72934cee64fc
Randall Pruim <rpruim@gmail.com>
2014-03-08 13:34:20 -0500

    reimplement ladd() using latticeExtra::layer()

--- R/ladd.R
+++ R/ladd.R
@@ -3,15 +3,12 @@
 #' Simplified lattice plotting by adding additional elements to existing plots.
 #'
 #' @param x  callable graphical element to be added to a panel or panels in a lattice plot 
-#' @param col,row identifies desired panel(s) in multi-panel plots.  If missing, all
-#'           columns or rows are used.
-#' @param verbose a logical indicating whether to display some information about modified panels.
-#' @param highlight a logical indicating whether to highlight panels as they are being modified.
+#' @param ... additional arguments passed to \code{\link[latticeExtra]{layer}}.
+#' @param plot a lattice plot to add to.  Defaults to previous lattice plot.
 #' 
 #' 
 #' @details
-#' \code{ladd} is simply a wrapper around 
-#' \code{\link{trellis.focus}} and \code{\link{trellis.unfocus}}. 
+#' \code{ladd} is simply a wrapper around \code{\link[latticeExtra]{layer}}.
 #' 
 #' @author Randall Pruim (\email{rpruim@@calvin.edu})
 #' 
@@ -28,7 +25,7 @@
 #' print(q)
 #' ladd(panel.abline(a=0,b=1))
 #' ladd(panel.abline(h=0,col='blue'))
-#' ladd( grid.text("(2,1)",gp=gpar(cex=3,alpha=.5)), 2, 1)
+#' ladd( grid.text("(2,1)",gp=gpar(cex=3,alpha=.5)), columns=2, rows=1)
 #' print(q)
 #' ladd( grid.text(paste(current.column(), current.row(),sep=','), gp=gpar(cex=3,alpha=.5)) )
 #' histogram( ~eruptions, data=faithful)
@@ -36,26 +33,9 @@
 #'
 #' @keywords graphics 
 
-ladd <- function (x, col, row, highlight=FALSE, verbose=FALSE) 
+ladd <- function (x, data=NULL, ..., plot=trellis.last.object()) 
 {
-	xUnevaluated <- substitute(x) 
-	layout <- trellis.currentLayout('panel')
-	if (missing(col)) { col <- 1:ncol(layout) }
-	if (missing(row)) { row <- 1:nrow(layout) }
-
-	for (r in row) {
-		for (c in col) {
-			if ( c >=1 && r >= 1 && c <= ncol(layout) && r <= nrow(layout) && layout[r,c] > 0 ) {
-				if (verbose) 
-					message(paste('  + Modifying panel ' ,layout[r,c], 
-								  ' at position (',c,',',r,')...',sep=""))        
-				trellis.focus("panel", c, r, highlight=highlight)
-				eval.parent(xUnevaluated, n=1)
-				trellis.unfocus()
-			} else {
-				if (verbose) 
-					message(paste('  * No panel at position (',c,',',r,').',sep=""))
-			}
-		}
-	}
+  return( plot + eval( substitute( layer(foo, data=data, ...),
+                                   list(foo=substitute(x) ) )
+  ) )
 }
