9757d9e2b15555d9e40fc439fe89cdbfc6e143b4
Randall Pruim <rpruim@gmail.com>
2014-04-11 13:00:33 -0400

    complete rewrite of ntiles

--- R/ntiles.R
+++ R/ntiles.R
@@ -3,6 +3,7 @@
 #' @param x a numeric vector
 #' @param n (approximate) number of quantiles
 #' @param format a specification of desired output format.
+#' @param digits desired number of digits for labeling of factors.
 #' @return a vector.  The type of vector will depend on \code{format}.
 #' @export
 #' @examples
@@ -12,29 +13,32 @@
 #' tally( ~ ntiles(1:50, 4, format="left") )
 #' tally( ~ ntiles(1:50, 4, format="right") )
 #' 
-ntiles <-  function(x, n=3, format=c("rank", "interval", "center", "left", "right")){
+ntiles <-  function(x, n=3, format=c("rank", "interval", "center", "left", "right"), digits=3){
   format <- match.arg(format)
+  
   # Figure out names
   qnames <- c("1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th")
   if (n > 10) qnames <- c(qnames, paste(11:n,"th",sep=""))
-  brks <- co.intervals( x, n, overlap=0 )
-  left <- brks[,1]
-  right <- brks[,2]
-  edge <- c( -Inf, ( tail(left,-1) + head(right,-1) ) / 2, Inf)
-  brks <- unique(edge)
-  left <- head(edge, -1)
-  right <- tail(edge, -1)
-  center <- ( left + right ) / 2
-  left[1] <- min(x, na.rm=TRUE)
-  right[length(right)] <- max(x, na.rm=TRUE)
-  center[1] <- left[1]
-  center[length(center)] <- right[length(right)]
-  res <- switch(format, 
-                "rank" =  cut(x, breaks=brks, labels=qnames[1:n], ordered_result=TRUE),
-                "interval" =  cut(x, breaks=brks, ordered_result=TRUE),
-                "center" = center[ as.numeric( cut(x, breaks=brks) ) ],
-                "left" = left[ as.numeric( cut(x, breaks=brks) ) ],
-                "right" = right[ as.numeric( cut(x, breaks=brks) ) ]
-  )
+  
+  xrank <- rank(x, na.last=TRUE)
+  xrank[is.na(x)] <- NA
+  size <- max(xrank, na.rm=TRUE)
+  cts <- round( seq(1, size, length.out = (n+1) ) )
+  bin <- as.numeric(cut( xrank, breaks = cts, include.lowest=TRUE ))
+  
+  left <- min( x ~ bin, na.rm=TRUE )
+  right <- max( x ~ bin, na.rm=TRUE )
+  center <- signif( ( left + right ) / 2, digits )
+  
+  res <- switch(format,
+                "rank" =  factor(bin, labels=qnames[1:n], ordered=TRUE),
+                "interval" =  factor(
+                  bin,  
+                  labels=paste0("[",signif(left,digits=digits),",", signif(right,digits=digits),"]"), 
+                  ordered=TRUE),
+                "center" = center[bin],
+                "left" = left[bin],
+                "right" = right[bin]
+  )                
   return(res)
 }
