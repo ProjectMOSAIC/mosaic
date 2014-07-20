5e48159e9cec31576648acae64dc470041b4ebea
bborges <bborgesr@macalester.edu>
2014-07-19 20:44:04 -0500

    Moved system.rda and improved documentation

--- R/maps2.R
+++ R/maps2.R
@@ -12,28 +12,50 @@
 #' the same region.  These utilities make it easier to merge data from different
 #' sources by converting names to standardized forms.
 #' 
+#' @details
+#' \describe{
+#' 
+#' \item{standardName}{This is the most general standardizing function.
+#' In addition to \code{x}, this function requires another argument:
+#' \code{standard}: a named vector in which each name is a particular
+#' spelling of the name in question and the value is the standardized
+#' version of that name}
+#'  
+#' \item{standardCountry}{This function will standardized the country
+#' names in \code{x} to the standard ISO_a3 country code format. If 
+#' \code{returnAlternatives} is set to \code{TRUE}, this function will also
+#' return the the named vector used to standardize the country names}
+#' 
+#' \item{standardState}{This function will standardized the US state
+#' names in \code{x} to the standard two-letter abbreviations. If 
+#' \code{returnAlternatives} is set to \code{TRUE}, this function will also
+#' return the the named vector used to standardize the state names}
+#' }
+#' 
 #' @param x A vector with the names to standardize
-#' @param standard A named vector in which each name is a particular spelling of
-#' the name in question and the value is the standardized version of that name 
 #' @export
 standardName <- function(x, standard) {
   standard[toupper(x)]
 }
 
-#' @param x A vector with the country names to standardize
-#' @return A vector with standardized country names (in the ISO-a3 format)
 #' @export
 #' @rdname standardName
-standardCountry <- function(x) {
-  standardName(x, countryAlternatives)
+standardCountry <- function(x, returnAlternatives = FALSE) {
+  stand <- standardName(x, countryAlternatives)
+  if (returnAlternatives == FALSE) return(stand)
+  else {
+    return(list(countries = stand, alternatives = countryAlternatives))
+  }
 }
 
-#' @param x A vector with the US state names to standardize
-#' @return A vector with standardized state names (as two-letter abbreviations)
 #' @export
 #' @rdname standardName
-standardState <- function(x) {
-  standardName(x, stateAlternatives)
+standardState <- function(x, returnAlternatives = FALSE) {
+  stand <- standardName(x, stateAlternatives)
+  if (returnAlternatives == FALSE) return(stand)
+  else {
+    return(list(countries = stand, alternatives = stateAlternatives))
+  }
 }
 
 #' @export
@@ -241,3 +263,53 @@ CIAdata <- function (name = NULL) {
                                table[[2]]))
   return(table)
 }
+
+
+
+tryCatch(utils::globalVariables(c('coordinates')),
+         error=function(e) message('Looks like you should update R.'))
+
+
+#' Transforms a shapefile into a dataframe
+#'
+#' This function takes in a shapefile (formal class of
+#' \code{SpatialPolygonsDataFrame}) and transforms it into a dataframe
+#'
+#' @param map A map object of class \code{SpatialPolygonsDataFrame}
+#' @param ... Other arguments, currently ignored
+#' @return A dataframe, in which the first 7 columns hold geographical
+#' information (ex: \code{long} and \code{lat})
+#' @examples
+#'
+#' \dontrun{ 
+#' if(require(maptools)) {
+#'   data(wrld_simpl)
+#'   worldmap <- sp2df(wrld_simpl)
+#' }
+#' 
+#' if ( require(ggplot2) && require(maptools) ) { 
+#'   data(wrld_simpl)
+#'   World <- sp2df(wrld_simpl)
+#'   World2 <- merge(World, Countries, by.x="NAME", by.y="maptools", all.y=FALSE)
+#'   Mdata <- merge(Alcohol, World2, by.x="country", by.y="gapminder", all.y=FALSE) 
+#'   Mdata <- Mdata[order(Mdata$order),]
+#'   qplot( x=long, y=lat, fill=ntiles(alcohol,5), 
+#'          data=subset(Mdata, year==2008), group = group, 
+#'                      geom="polygon")
+#' }
+#' }
+#' @export
+
+sp2df <- function (map, ...) 
+{
+  .try_require(c("ggplot2", "maptools")) 
+  map@data$id <- rownames(map@data)
+  coords_matrix <- coordinates(map)  # in sp, which maptools depends on
+  map@data$clon = coords_matrix[, 1]
+  map@data$clat = coords_matrix[, 2]
+  map_points <- do.call(fortify, list(map, region="id"), 
+                        envir=environment(ggplot2::fortify))
+  result <- merge(map_points, map@data, by = "id")
+  return(result)
+}
+
