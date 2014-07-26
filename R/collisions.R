07272879c5ed5e891b296dcf8d525a5799a0447e
Randall Pruim <rpruim@gmail.com>
2014-07-26 19:34:42 -0400

    don't check for dplyr/MASS; using ugly ::: method -- need to improve this.

--- R/collisions.R
+++ R/collisions.R
@@ -1,7 +1,7 @@
 #' Avoiding inter-package collisions
 #' 
 #' Several popular packages export functions with the same names.  The \pkg{mosaic}
-#' package attempts to help these other packge co-exist by creating generic functions
+#' package attempts to help these other package co-exist by creating generic functions
 #' with the same name and dispatching to the appropriate function based on the (first)
 #' arguemnt supplied.
 #' 
@@ -9,6 +9,7 @@
 #' @name collisions
 #' 
 #' @param object and R object
+#' @param ... additional arguments
 #' @export
 select <- function(object, ...) {
   UseMethod("select")
@@ -18,27 +19,28 @@ select <- function(object, ...) {
 #' @name collisions
 #' @export
 select.tbl <- function(object, ...) {
-  if (! "packge:dplyr" %in% search()) {
-    stop("The `dplyr' package needs to be installed and loaded to use select this way.")
-  }
-  dplyr::select(.data=object, ...)
+  dplyr:::select.tbl(object, ...)
+}
+
+#' @rdname collisions
+#' @name collisions
+#' @export
+select.data.frame  <- function(object, ...) {
+  dplyr:::select.data.frame(object, ...)  
 }
 
 #' @rdname collisions
 #' @name collisions
 #' @export
 select.ridgelm <- function(object, ...) {
-  if (! "packge:MASS" %in% search()) {
-    stop("The `MASS' package needs to be installed and loaded to use select this way.")
-  }
-  MASS::select(formula=object, ...)
+  MASS:::select.ridgelm(formula=object, ...)
 }
 
 #' @rdname collisions
 #' @name collisions
 #' @export
 select.default <- function(object, ...) {
-  message("mosaic is aware of select functions in the MASS and dplyr packages.")
-  message("But your usage does not conform to either of these.  If you are trying")
+  message("mosaic is aware of select functions in the utils, MASS and dplyr packages.")
+  message("But your usage does not appear to conform to one of these.  If you are trying")
   message("to use some other select, please use a fully qualified name.")
 }
