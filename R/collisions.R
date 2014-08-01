e9a0c2aef5c2d7208bd21bb88404eebf5a3dc23b
Randall Pruim <rpruim@gmail.com>
2014-08-01 10:26:00 -0400

    still dealing with select() -- still not perfect; arghh.

--- R/collisions.R
+++ R/collisions.R
@@ -4,15 +4,19 @@
 #' Select a subset of columns using \code{dplyr::select}.
 #' 
 #' @rdname select
+# #' @param .data a data frame or object that inherits from class \code{"tbl"}
+#' @param ... addtional arguments passed to \code{dplyr::select}.
 #' @details
 #' Both the \pkg{MASS} and \pkg{dplyr} packages define a generic
 #' \code{select}.  When the \pkg{mosaic} package is loaded, 
 #' \code{dplyr::select} will be preferred.
 #' 
-#' @seealso \code{\pkg{dplyr::}\link[dplyr]{select}} 
-#' and  \code{\pkg{MASS::}\link[MASS]{select}} 
+#' @seealso \code{dplyr::\link[dplyr]{select}} 
+#' and  \code{MASS::\link[MASS]{select}} 
 #' @examples
 #' HELPrct %>% sample(5) %>% select(sex, substance, homeless)
 #' @export
 #' 
-select <- dplyr::select
+select <- function(...) {
+  dplyr::select(...)
+}
