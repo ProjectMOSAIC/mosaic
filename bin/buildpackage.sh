d02bcca2272821e21fdc7142220bc072e5064a79
Randall Pruim <rpruim@gmail.com>
2014-08-26 23:13:22 -0400

    stop ignoring builds, move to RBuildIgnore

--- bin/buildpackage.sh
+++ bin/buildpackage.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 export _R_CHECK_FORCE_SUGGESTS_=false
 bin/roxy -p "." --clean
-mv *_*.tar.gz builds/
+mv *_*.tar.gz RBuildIngore/builds/
 #\cp sandbox/Vignettes/Resampling/Resampling.pdf inst/doc/Resampling.pdf
 #\cp sandbox/Vignettes/Calculus/mosaic-calculus.pdf inst/doc/Calculus.pdf
 #\cp sandbox/Vignettes/MinimalR/MinimalR.pdf inst/doc/MinimalR.pdf
