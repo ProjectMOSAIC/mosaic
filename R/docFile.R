ce9a598f1d9f31b0c27724f15ba0826517a32fb6
Randall Pruim <rpruim@gmail.com>
2014-09-03 19:32:09 -0400

    construct path to a file in the documentation section of a package

---
+++ R/docFile.R
@@ -0,0 +1,17 @@
+
+#' Return the path to a documentation file in a package
+#' 
+#' Return the path to a documentation file in a package
+#' 
+#' @param file the name of a file
+#' @param package the name of a package
+#' @return a character vector specifying the path to the file on the user's system.
+#' @export
+#' @examples
+#' MustangPrice <- read.file(docFile("MustangPrice", "mosaic"))
+
+docFile <- function(file, package="mosaic", character.only=FALSE) {
+  if (! character.only)  package <- as.character(substitute(package))
+  path <- find.package(package, verbose=FALSE)
+  return(file.path(path,"doc",file))
+}
