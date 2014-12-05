5bb42d2e491bc553f73bf19a718e465941f355bb
Randall Pruim <rpruim@gmail.com>
2014-12-05 09:55:00 -0500

    typo: link -> \link

--- R/collisions.R
+++ R/collisions.R
@@ -2,10 +2,10 @@
 #' Maximum-likelihood fitting of univariate distributions
 #' 
 #' Maximum-likelihood fitting of univariate distributions (from \pkg{MASS})
-#' @param x see \code{link[MASS]{fitdistr}} in the \pkg{MASS} package
-#' @param densfun see \code{link[MASS]{fitdistr}} in the \pkg{MASS} package
-#' @param start see \code{link[MASS]{fitdistr}} in the \pkg{MASS} package
-#' @param ... see \code{link[MASS]{fitdistr}} in the \pkg{MASS} package
+#' @param x see \code{\link[MASS]{fitdistr}} in the \pkg{MASS} package
+#' @param densfun see \code{\link[MASS]{fitdistr}} in the \pkg{MASS} package
+#' @param start see \code{\link[MASS]{fitdistr}} in the \pkg{MASS} package
+#' @param ... see \code{\link[MASS]{fitdistr}} in the \pkg{MASS} package
 #' @export
 fitdistr <- MASS::fitdistr
 
@@ -13,9 +13,9 @@ fitdistr <- MASS::fitdistr
 #' 
 #' Rational Approximation from \pkg{MASS}.
 #' 
-#' @param x see \code{link[MASS]{fractions}} in the \pkg{MASS} package
-#' @param cycles see \code{link[MASS]{fractions}} in the \pkg{MASS} package
-#' @param max.denominator see \code{link[MASS]{fractions}} in the \pkg{MASS} package
-#' @param ... see \code{link[MASS]{fractions}} in the \pkg{MASS} package
+#' @param x see \code{\link[MASS]{fractions}} in the \pkg{MASS} package
+#' @param cycles see \code{\link[MASS]{fractions}} in the \pkg{MASS} package
+#' @param max.denominator see \code{\link[MASS]{fractions}} in the \pkg{MASS} package
+#' @param ... see \code{\link[MASS]{fractions}} in the \pkg{MASS} package
 #' @export
 fractions <- MASS::fractions
