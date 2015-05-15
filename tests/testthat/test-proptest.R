0e62263c72aba6f60965feb2bf856fbae5877e7b
Randall Pruim <rpruim@gmail.com>
2015-05-15 21:16:38 +1200

    redo tests -- more like binom.test tests

--- tests/testthat/test-proptest.R
+++ tests/testthat/test-proptest.R
@@ -9,79 +9,78 @@ TestData <- data.frame( a = factor(rep(letters[1:3], length.out=100)),
 
 test_that("formulas work", {
   
-  expect_equivalent( 
-    interval(stats::prop.test(34, 100)),
-    interval(prop.test(~ a, data=TestData))
-  )
+  X <- stats::prop.test(34, 100)
+  A <- prop.test(~ a, data=TestData)
+  B <- prop.test(~ b, data=TestData)
+  C <- prop.test(~ c, data=TestData)
   
-  expect_equivalent( 
-    interval(stats::prop.test(34, 100)),
-    interval(prop.test(~ b, data=TestData))
-  )
+  expect_equivalent(interval(A), interval(X))
+  expect_equivalent(A$data.name, "TestData$a")
   
-  expect_equivalent( 
-    interval(stats::prop.test(34, 100)),
-    interval(prop.test(~ c, data=TestData))
-  )
+  expect_equivalent(interval(B), interval(X))
+  expect_equivalent(B$data.name, "TestData$b")
+  
+  expect_equivalent(interval(C), interval(X))
+  expect_equivalent(C$data.name, "TestData$c")
+  
 })
 
 test_that("formulas work with unnamed second arg", {
   
-  expect_equivalent( 
-    interval(stats::prop.test(34, 100)),
-    interval(prop.test(~ a, TestData))
-  )
+  X <- stats::prop.test(34, 100)
+  A <- prop.test(~ a, TestData)
+  B <- prop.test(~ b, TestData)
+  C <- prop.test(~ c, TestData)
   
-  expect_equivalent( 
-    interval(stats::prop.test(34, 100)),
-    interval(prop.test(~ b, TestData))
-  )
+  expect_equivalent(interval(A), interval(X))
+  expect_equivalent(A$data.name, "TestData$a")
   
-  expect_equivalent( 
-    interval(stats::prop.test(34, 100)),
-    interval(prop.test(~ c, TestData))
-  )
+  expect_equivalent(interval(B), interval(X))
+  expect_equivalent(B$data.name, "TestData$b")
+  
+  expect_equivalent(interval(C), interval(X))
+  expect_equivalent(C$data.name, "TestData$c")
 })
+
 test_that("success = works", {
-  expect_equivalent( 
-    interval(stats::prop.test(33, 100)),
-    interval(prop.test(~ a, data=TestData, success="b"))
-  )
   
-  expect_equivalent( 
-    interval(stats::prop.test(33, 100)),
-    interval(prop.test(~ b, data=TestData, success="b"))
-  )
+  X <- stats::prop.test(33, 100)
+  Y <- stats::prop.test(66, 100)
+  A <- prop.test(~ a, data=TestData, success = "b")
+  B <- prop.test(~ b, data=TestData, success = "b")
+  C <- prop.test(~ c, data=TestData, success = FALSE)
   
-  expect_equivalent( 
-    interval(stats::prop.test(66, 100)),
-    interval(prop.test(~ c, data=TestData, success=FALSE))
-  )
+  expect_equivalent(interval(A), interval(X))
+  expect_equivalent(A$data.name, "TestData$a")
   
+  expect_equivalent(interval(B), interval(X))
+  expect_equivalent(B$data.name, "TestData$b")
+  
+  expect_equivalent(interval(C), interval(Y))
+  expect_equivalent(C$data.name, "TestData$c")
 })
 
 
 test_that("bare vars work", {
-  expect_equivalent( 
-    interval(stats::prop.test(33, 100)),
-    interval(prop.test(a, data=TestData, success="b"))
-  )
+  X <- stats::prop.test(34, 100)
+  A <- prop.test( a, data=TestData)
+  B <- prop.test( b, data=TestData)
+  C <- prop.test( c, data=TestData)
   
-  expect_equivalent( 
-    interval(stats::prop.test(33, 100)),
-    interval(prop.test(b, data=TestData, success="b"))
-  )
+  expect_equivalent(interval(A), interval(X))
+  expect_equivalent(A$data.name, "a")
   
-  expect_equivalent( 
-    interval(stats::prop.test(34, 100)),
-    interval(prop.test(c, data=TestData))
-  )
+  expect_equivalent(interval(B), interval(X))
+  expect_equivalent(B$data.name, "b")
+  
+  expect_equivalent(interval(C), interval(X))
+  expect_equivalent(C$data.name, "c")
 })
 
 test_that("numbers work", {
   expect_equivalent( 
     interval(stats::prop.test(33, 100)),
-    interval(prop.test(33,100))
+    interval(prop.test(33, 100))
   )
   
 })
