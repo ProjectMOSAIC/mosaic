8409b5fa873492ad5eee488d3922ab1493192a86
Randall Pruim <rpruim@gmail.com>
2015-07-28 14:21:26 -0400

    remove examples since perctable() is deprecated.

--- R/perctable.R
+++ R/perctable.R
@@ -11,11 +11,8 @@
 #' @return a contingency table, an object of class "table", an array of percentage or proportion
 #' values. Note that unlike S the result is always an array, a 1D array if one factor is given.
 #' @examples
-#' perctable(rbinom(1000,10,.5))
-#' with(airquality,
-#'    perctable(OzHi=Ozone > 80, Month, useNA="ifany"))
-#' with(airquality,
-#'    perctable(OzHi=Ozone > 80, Month, useNA="always"))
+#' # perctable() has been deprecated.  Use tally() instead
+#' example(tally)
 #' 
 #' @keywords manipulate 
 #' @export
