e30313a648682e052628aa9d4d0af8307039c3a6
Randall Pruim <rpruim@gmail.com>
2015-09-21 23:50:00 -0400

    axis.ticks.margin deprecated, use new method

--- R/maps2.R
+++ R/maps2.R
@@ -388,10 +388,11 @@ theme_map <- function (base_size=12) {
     theme(
       axis.title = element_blank (),
       axis.text = element_blank (),
+      axis.text = element_text(margin = unit(0, "cm")),
       panel.background = element_blank (),
       panel.grid = element_blank (),
       axis.ticks.length = unit (0,"cm"),
-      axis.ticks.margin = unit (0.01,"cm"),
+      # axis.ticks.margin = unit (0.01,"cm"),
       panel.margin = unit (0,"lines"),
       plot.margin = unit(c(0,0,0,0),"lines"),
       complete = TRUE
