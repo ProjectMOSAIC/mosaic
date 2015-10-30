5e812db5774e474a51273fb106fd6894749f645f
Randall Pruim <rpruim@gmail.com>
2015-10-30 17:17:13 -0400

    update tests for #537.

--- tests/testthat/test-pqrdata.R
+++ tests/testthat/test-pqrdata.R
@@ -3,25 +3,26 @@ context('pdata(), qdata(), and rdata()')
 
 
 test_that("formula interface works.", {
-	expect_equivalent( length(rdata( 10, ~age, mosaicData::HELPrct)), 10 )
-	expect_equivalent( pdata(30, ~age, mosaicData::HELPrct), prop( ~ mosaicData::HELPrct$age <= 30) )
-	expect_equivalent( qdata(.75, ~age, mosaicData::HELPrct)['quantile'], quantile(mosaicData::HELPrct$age, .75) )
-	expect_equivalent( qdata(c(.25,.75), ~age, mosaicData::HELPrct)$quantile, quantile(mosaicData::HELPrct$age, c(.25,.75)) )
+	expect_equivalent( length(rdata( ~age, 10, data = mosaicData::HELPrct)), 10 )
+	expect_equivalent( pdata( ~age, 30, data = mosaicData::HELPrct), prop( ~ mosaicData::HELPrct$age <= 30) )
+	expect_equivalent( qdata( ~age, .75, mosaicData::HELPrct)['quantile'], quantile(mosaicData::HELPrct$age, .75) )
+	expect_equivalent( qdata(~ age, c(.25,.75), mosaicData::HELPrct)$quantile, quantile(mosaicData::HELPrct$age, c(.25,.75)) )
 })
 
 test_that("vector/data interface works.", {
-	expect_equivalent( length(rdata( 10, age, mosaicData::HELPrct)), 10 )
-	expect_equivalent( pdata(30, age, data=mosaicData::HELPrct), prop(~ mosaicData::HELPrct$age <= 30) )
-	expect_equivalent( qdata(.75, age, data=mosaicData::HELPrct)['quantile'], quantile(mosaicData::HELPrct$age, .75) )
-	expect_equivalent( qdata(c(.25,.75), age, mosaicData::HELPrct)$quantile, quantile(mosaicData::HELPrct$age, c(.25,.75)) )
+	expect_equivalent( length(rdata( age, 10, mosaicData::HELPrct)), 10 )
+	expect_equivalent( pdata(age, 30, data = mosaicData::HELPrct), prop(~ mosaicData::HELPrct$age <= 30) )
+	expect_equivalent( qdata(age, .75, data = mosaicData::HELPrct)['quantile'], quantile(mosaicData::HELPrct$age, .75) )
+	expect_equivalent( qdata(~age, c(.25,.75), data = mosaicData::HELPrct)$quantile, quantile(mosaicData::HELPrct$age, c(.25,.75)) )
 })
 
 
 test_that("vector (no data) interface works.", {
-	expect_equivalent( length(rdata( 10, mosaicData::HELPrct$age)), 10 )
-	expect_equivalent( pdata(30, mosaicData::HELPrct$age), prop( ~ mosaicData::HELPrct$age <= 30) )
-	expect_equivalent( qdata(.75, mosaicData::HELPrct$age)['quantile'], quantile(mosaicData::HELPrct$age, .75) )
-	expect_equivalent( qdata(c(.25,.75), mosaicData::HELPrct$age)$quantile, quantile(mosaicData::HELPrct$age, c(.25,.75)) )
+	expect_equivalent( length(rdata( n = 10, mosaicData::HELPrct$age)), 10 )
+	expect_equivalent( pdata(mosaicData::HELPrct$age, 30), prop( ~ mosaicData::HELPrct$age <= 30) )
+	expect_equivalent( qdata(mosaicData::HELPrct$age, .75)['quantile'], quantile(mosaicData::HELPrct$age, .75) )
+	expect_equivalent( qdata( p = c(.25,.75), mosaicData::HELPrct$age)$quantile, 
+	                   quantile(mosaicData::HELPrct$age, c(.25,.75)) )
 })
 
 #test_that("error messages generated", {
