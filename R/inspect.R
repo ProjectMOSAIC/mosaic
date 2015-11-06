6a09b4643ff7f23cc1e21f9b6d92a7884f0063f8
Randall Pruim <rpruim@gmail.com>
2015-11-06 12:03:26 -0500

    add inspect.list, remove inspect.default

--- R/inspect.R
+++ R/inspect.R
@@ -12,8 +12,8 @@ inspect <- function(object, ...) {
 }
 
 #' @export
-inspect.default <- function(object, ...) {
-  NA
+inspect.list <- function(object, max.level = 2, ...) {
+  str(object, max.level = max.level, ...)
 }
 
 #' @export
@@ -67,18 +67,30 @@ inspect.POSIXt <- function(object, ...) {
 }
 
 #' @export
-inspect.data.frame <- function(object, select = TRUE, digits = 3, ...) {
+inspect.data.frame <- function(object, select = TRUE, digits = getOption("digits", 3), ...) {
   L <- dfapply(object, inspect, select = select, ...)
   classes <- sapply( L, function(x) if(is.null(x[["class"]])) "" else x[["class"]] ) 
   classes[classes %in% c("numeric", "integer")] <- "quantitative"
   classes[classes %in% c("factor", "ordered", "logical", "character")] <- "categorical"
   classes[classes %in% c("POSIXt", "POSIXct")] <- "time"
   uclasses <- sort(unique(classes))
- 
+  res <- list()
   for (class in uclasses) {
     idx <- which(classes == class)
-    cat("\n")
-    cat(paste0(class, " variables:  \n"))
-    print( bind_cols(data_frame(name = names(L[idx])), bind_rows(L[idx])) %>% as.data.frame(), digits = digits)
+    res[[class]] <- 
+    bind_cols(data_frame(name = names(L[idx])), bind_rows(L[idx]))
   }
+  
+  structure(res, class = "inspected_data_frame", digits = digits)
+}
+
+#'@export
+print.inspected_data_frame <- function(x, digits = NULL, ...) {
+  if (is.null(digits)) digits <- attr(x, "digits")
+  for (n in names(x)) {
+    cat("\n")
+    cat(paste0(n, " variables:  \n"))
+    print(as.data.frame(x[[n]]), digits = digits)
+  }
+  invisible(x)
 }
