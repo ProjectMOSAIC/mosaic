b534f52a406bec4c40b600f57170f85d475f5ef0
Randall Pruim <rpruim@gmail.com>
2015-11-12 22:36:23 -0500

    Improvements to select argument of dfapply() and inspect().

--- R/dfapply.R
+++ R/dfapply.R
@@ -4,21 +4,24 @@
 #' @param data  data frame 
 #' @param FUN 
 #'   a function to apply to (some) variables in the data frame
-#' @param select 
-#'   function used to select variables to which \code{FUN} is applied.  See examples.
+#' @param select a logical, character (naming variables), or numeric vector or a 
+#'   function used to select variables to which \code{FUN} is applied.  If a function,
+#'   it should take a vector as input and return a single logical. See examples.
 #' @param \dots arguments passed along to \code{FUN} 
 #' 
 #' @seealso \code{\link{apply}},
 #' \code{\link{sapply}},
 #' \code{\link{tapply}},
-#' \code{\link{lapply}}
+#' \code{\link{lapply}},
+#' \code{\link{inspect}}
 #' 
 #' @examples
-#' dfapply(iris, favstats)
-#' if (require(mosaicData)) {
-#'   dfapply(HELPrct, table, select=is.factor)
-#'   do.call(rbind, dfapply(HELPrct, favstats, select=is.numeric))
-#' }
+#' dfapply(iris, favstats, select = is.numeric)
+#' dfapply(iris, favstats, select = c(TRUE, TRUE, FALSE, FALSE, FALSE))
+#' dfapply(iris, favstats, select = c(1,2))
+#' dfapply(iris, favstats, select = c("Sepal.Length", "Petal.Length"))
+#' dfapply(HELPrct, table, select = is.factor)
+#' do.call(rbind, dfapply(HELPrct, favstats, select = is.numeric))
 #' @export
 
 dfapply <-
@@ -30,6 +33,9 @@ function (data, FUN, select = TRUE, ...)
     if (is.logical(select)) {
         select <- rep(select, length.out = dim(data)[2])
         select <- which(select)
+    }
+    if (is.character(select)) {
+       select <- sapply(select, function(x) which (x == names(data)))
     }
     if (!is.numeric(select)) {
         stop("Unusable selection parameter.")
--- R/inspect.R
+++ R/inspect.R
@@ -3,29 +3,43 @@
 #' Print a short summary of the contents of an object.  Most useful as a way to
 #' get a quick overview of the variables in data frame.
 #' 
+#' @param object a data frame or a vector
+#' @param additional arguments passed along to methods
+#' @param max.level an integer giving the depth to which lists should be expanded
+#' @param digits and integer giving the number of digits to display
+#' @param select a logical, character (naming variables), or numeric vector or a 
+#'   function used to select variables to which \code{FUN} is applied.  If a function,
+#'   it should take a vector as input and return a single logical. See examples here and
+#'   at \code{link{dfapply}}.
+#'   
 #' @export
 #' @examples
 #' inspect(Births78)
-#' 
+#' inspect(Births78, is.numeric)
+ 
 inspect <- function(object, ...) {
   UseMethod("inspect")
 }
 
+#' @rdname inspect
 #' @export
 inspect.list <- function(object, max.level = 2, ...) {
   str(object, max.level = max.level, ...)
 }
 
+#' @rdname inspect
 #' @export
 inspect.character <- function(object, ...) {
   inspect(factor(object)) %>% mutate(class = "character")
 }
 
+#' @rdname inspect
 #' @export
 inspect.logical <- function(object, ...) {
   inspect(as.character(object, ...)) %>% mutate(class = "logical")
 }
 
+#' @rdname inspect
 #' @export
 inspect.numeric <- function(object, ...) {
   bind_cols(
@@ -34,6 +48,7 @@ inspect.numeric <- function(object, ...) {
     )
 }
 
+#' @rdname inspect
 #' @export
 inspect.factor <- function(object, ...) {
   tbl <- sort(table(object), decreasing = TRUE) 
@@ -53,6 +68,7 @@ inspect.factor <- function(object, ...) {
   )
 }
 
+#' @rdname inspect
 #' @export
 inspect.POSIXt <- function(object, ...) {
   data_frame(
@@ -66,6 +82,7 @@ inspect.POSIXt <- function(object, ...) {
   )
 }
 
+#' @rdname inspect
 #' @export
 inspect.data.frame <- function(object, select = TRUE, digits = getOption("digits", 3), ...) {
   L <- dfapply(object, inspect, select = select, ...)
@@ -84,6 +101,7 @@ inspect.data.frame <- function(object, select = TRUE, digits = getOption("digits
   structure(res, class = "inspected_data_frame", digits = digits)
 }
 
+#' @rdname inspect
 #'@export
 print.inspected_data_frame <- function(x, digits = NULL, ...) {
   if (is.null(digits)) digits <- attr(x, "digits")
