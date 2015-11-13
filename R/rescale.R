630fe1a8c541e9abfaaf42f1adbd56a2decca40a
Randall Pruim <rpruim@gmail.com>
2015-11-12 21:06:00 -0500

    use NULL as default value for domain

--- R/rescale.R
+++ R/rescale.R
@@ -6,37 +6,40 @@
 #' parallel plots or heatmaps.
 #' @rdname rescale
 #' @param x an R object to rescale
-#' @param domain a numeric vector of length 2
+#' @param domain a numeric vector of length 2 or \code{NULL}
 #' @param range a numeric vector of length 2
 #' @param ... additional arguments
 #' @export 
-rescale <- function(x, range, domain, ...) {
+rescale <- function(x, range, domain = NULL, ...) {
   UseMethod("rescale")
 }
 
 #' @rdname rescale
 #' @export
 rescale.data.frame <- function( x, 
-                                range=c(0,1), 
-                                domain,
+                                range = c(0,1), 
+                                domain = NULL,
                                 ...) {
   dfapply( x, 
-           function(x) { rescale(x, range=range, domain=domain, ...) },
+           function(x) { rescale(x, range = range, domain = domain, ...) },
            function(x) TRUE ) %>%
     as.data.frame()
 }
 
 #' @rdname rescale
 #' @export
-rescale.factor <- function(x, range, domain=range(1:nlevels(x)), ...) {
-  width <- diff(range)
-  n <- length(levels(x)) - 1
-  range[1]  - 1/n + width * as.numeric(x) / n
+rescale.factor <- function(x, range, domain = NULL, ...) {
+  if( is.null(domain)) domain <- range(1:nlevels(x))
+  rescale( as.numeric(x), domain = domain, range = range)
+#  width <- diff(range)
+#  n <- length(levels(x)) - 1
+#  range[1]  - 1/n + width * as.numeric(x) / n
 }
 
 #' @rdname rescale
 #' @export
-rescale.numeric <- function(x, range=c(0,1), domain=range(x, na.rm=TRUE), ...) {
+rescale.numeric <- function(x, range = c(0,1), domain = NULL, ...) {
+  if (is.null(domain)) domain <- range(x, na.rm = TRUE)
   range_width  <- diff(range)
   domain_width <- diff(domain)
   range[1] + range_width * (x - min(x)) / domain_width
@@ -44,12 +47,12 @@ rescale.numeric <- function(x, range=c(0,1), domain=range(x, na.rm=TRUE), ...) {
 
 #' @rdname rescale
 #' @export
-rescale.default <- function(x, range=c(0,1), domain, ...) {
-  rescale( as.numeric(x, range=range, domain, ...) )
+rescale.default <- function(x, range = c(0,1), domain = NULL, ...) {
+  rescale( as.numeric(x, range = range, domain = domain, ...) )
 }
 
 #' @rdname rescale
 #' @export
-rescale.character <- function(x, range=c(0,1), domain, ...) {
-  rescale( as.factor(x), range=range, domain=domain)
+rescale.character <- function(x, range=c(0,1), domain = NULL, ...) {
+  rescale( as.factor(x), range = range, domain = domain)
 }
