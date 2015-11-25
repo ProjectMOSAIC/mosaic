ee236b34baeeac6e0d1d19365b6bbf46d36d5057
Randall Pruim <rpruim@gmail.com>
2015-11-25 16:13:09 -0500

    add more functionality to chisq()

--- R/chisq.R
+++ R/chisq.R
@@ -3,8 +3,12 @@
 #' 
 #' Extract Chi-squared statistic
 #' 
-#' @param x,... Either an object of class \code{"htest"} coming from a Chi-squared test or
-#' the inputs to \code{\link{chisq.test}}.
+#' @param x An object of class \code{"htest"} a coming from a Chi-squared test,
+#' an object of class \code{"table"}, or
+#' the inputs to \code{\link{tally}}.
+#' @param correct a logical indicating whether a continuity correction should be 
+#' applied.
+#' @param ... additional arguments passed on to \code{tally} or \code{chisq.test}.
 #' @seealso \code{\link{stat}}
 #' @examples
 #' 
@@ -31,11 +35,23 @@ chisq.htest <- function(x, ...) {
   if (! grepl ("Chi-squared", x$method) ) {
     stop("This doesn't look like a Chi-sqaured test to me.")
   }
-  return(x$statistic)
+  setNames(x$statistic, "X.squared")
+}
+
+#' @rdname chisq
+#' @export
+chisq.table <- function(x, correct = FALSE, ...) {
+  setNames(
+    chisq.test(x, correct = correct, ...)$statistic,
+    "X.squared"
+  )
 }
   
 #' @rdname chisq
 #' @export
-chisq.default <- function(x, ...) {
-  chisq.test(x, ...)$statistic
+chisq.default <- function(x, correct = FALSE, ...) {
+  setNames(
+    chisq.test(tally(x, ...), correct = correct)$statistic,
+    "X.squared"
+  )
 }
