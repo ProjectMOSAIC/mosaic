6552559d346d0a856d07e7f036de4c83867374d5
Randall Pruim <rpruim@gmail.com>
2016-05-18 10:35:22 -0400

    handle rexports a bit differently.

---
+++ R/reexports.R
@@ -0,0 +1,11 @@
+
+# This allows us to use these functions from MASS without attaching the MASS package.
+
+#' 
+#' @importFrom MASS fitdistr
+#' @export
+MASS::fitdistr
+
+#' @importFrom MASS fractions
+#' @export
+MASS::fractions
