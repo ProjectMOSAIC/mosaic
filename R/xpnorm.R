ee5e9028e4f642568564c71edc00b150ed6d94a1
Randall Pruim <rpruim@gmail.com>
2017-07-18 18:02:31 -0400

    convert xpnorm() and xqnorm() to ggplot2

--- R/xpnorm.R
+++ R/xpnorm.R
@@ -51,8 +51,10 @@ xpnorm <-
 function (q, mean = 0, sd = 1, plot = TRUE, verbose = TRUE, invisible=FALSE, digits = 4, 
     lower.tail = TRUE, log.p = FALSE, xlim = mean + c(-4,4) * sd, ylim = c(0, 1.4 * dnorm(mean,mean,sd)), 
     vlwd=2, vcol=trellis.par.get('add.line')$col,
-	rot=45, manipulate=FALSE, ...) 
+	rot=45, manipulate=FALSE, ..., return = c("value", "plot")) 
 {
+  return <- match.arg(return)
+  
 	if (manipulate && rstudio_is_available() && requireNamespace("manipulate")) {
 		return(manipulate::manipulate( 
 			xpnorm(q=Q, mean=MEAN, sd=SD, plot=TRUE, verbose=FALSE, invisible=invisible,
@@ -68,29 +70,38 @@ function (q, mean = 0, sd = 1, plot = TRUE, verbose = TRUE, invisible=FALSE, dig
     p = pnorm(q, mean = mean, sd = sd) 
     z = (q - mean)/sd
     if (verbose) {
-		cat("\n")
-		cat(paste("If X ~ N(",format(mean, digits = digits),", ", 
-		          format(sd, digits = digits),"), then \n\n",sep=""))
-        cat(paste("\tP(X <= ", format(q, digits = digits), ") = P(Z <= ", 
+		message("\n")
+		message(paste("If X ~ N(",format(mean, digits = digits),", ", 
+		          format(sd, digits = digits),"), then \n",sep=""))
+        message(paste("\tP(X <= ", format(q, digits = digits), ") = P(Z <= ", 
                   format(z, digits = digits), 
-            ") = ", format(p, digits = digits), "\n", sep = ""))
-        cat(paste("\tP(X >  ", format(q, digits = digits), ") = P(Z >  ", 
+            ") = ", format(p, digits = digits), "", sep = ""))
+        message(paste("\tP(X >  ", format(q, digits = digits), ") = P(Z >  ", 
                   format(z, digits = digits), 
-                  ") = ", format(1 - p, digits = digits), "\n", sep = ""))
-        cat("\n")
+                  ") = ", format(1 - p, digits = digits), "", sep = ""))
+        message("\n")
     }
     if (plot & length(q) == 1) {
-		print(.plot_one_norm(p=p, q=q, mean, sd, xlim=xlim, ylim=ylim, digits=digits, 
-		      vlwd=vlwd, vcol=vcol, rot=rot, ...))
+		  res_plot <- .plot_one_norm(p=p, q=q, mean, sd, xlim=xlim, ylim=ylim, digits=digits, 
+		      vlwd=vlwd, vcol=vcol, rot=rot, ...)
     }
     if (plot & length(q) > 1) {
-			print(.plot_multi_norm(p=p, q=q, mean, sd, xlim=xlim, ylim=ylim, digits=digits, 
-		      vlwd=vlwd, vcol=vcol, rot=rot, ...))
+      res_plot <- 
+        .plot_multi_norm(p=p, q=q, mean, sd, xlim=xlim, ylim=ylim, digits=digits, 
+                         vlwd=vlwd, vcol=vcol, rot=rot, ...)
     }
-	if (invisible) { 
-    	return(invisible(pnorm(q, mean = mean, sd = sd, lower.tail = lower.tail, log.p = log.p)))
-	}
-    return(pnorm(q, mean = mean, sd = sd, lower.tail = lower.tail, log.p = log.p))
+    if (return == "plot") {
+      if (invisible) {
+        return(invisible(res_plot))
+      } else {
+        return(res_plot)
+      }
+    } else {
+      if (invisible) { 
+        return(invisible(pnorm(q, mean = mean, sd = sd, lower.tail = lower.tail, log.p = log.p)))
+      }
+      return(pnorm(q, mean = mean, sd = sd, lower.tail = lower.tail, log.p = log.p))
+    }
 }
 
 #' @rdname xpnorm
@@ -107,12 +118,12 @@ function (p, mean = 0, sd = 1, plot = TRUE, verbose = TRUE, digits = getOption("
     z = (q - mean)/sd
     p <- pnorm(z)
     if (verbose) {
-      cat("\n")
-      cat(paste("If X ~ N(",format(mean, digits = digits),", ", 
-                format(sd, digits = digits),"), then \n\n",sep=""))
-      cat(paste("\tP(X", " <= ", format(q, digits = digits), ") = ", p, "\n", sep = ""))
-      cat(paste("\tP(X", " >  ", format(q, digits = digits), ") = ", 1 - p, "\n", sep = ""))
-      cat("\n")
+      message("\n")
+      message(paste("If X ~ N(",format(mean, digits = digits),", ", 
+                format(sd, digits = digits),"), then \n",sep=""))
+      message(paste("\tP(X", " <= ", format(q, digits = digits), ") = ", p, "", sep = ""))
+      message(paste("\tP(X", " >  ", format(q, digits = digits), ") = ", 1 - p, "", sep = ""))
+      message("\n")
     }
     if (! lower.tail) { # make sure we have lower tail probs from here on
       p <- 1 - p
@@ -126,10 +137,19 @@ function (p, mean = 0, sd = 1, plot = TRUE, verbose = TRUE, digits = getOption("
 		print(.plot_multi_norm(p=sort(p), q=sort(q), mean, sd, xlim=xlim, ylim=ylim, digits=digits, 
 		      vlwd=vlwd, vcol=vcol, rot=rot, ...))
     }
-	if (invisible) { 
-    		return(invisible(q))
-	}
-    return(q)
+    
+    if (return == "plot") {
+      if (invisible) {
+        return(invisible(res_plot))
+      } else {
+        return(res_plot)
+      }
+    } else {
+      if (invisible) { 
+        return(invisible(q))
+      }
+      return(q)
+    }
 }
 
 #' midpoints along a sequence
@@ -219,72 +239,39 @@ mid <- function(x) {
 	...) 
 {
 	z <- (q - mean) / sd
-			z <- (q - mean) / sd 
 	zmax = max(4, abs(z) * 1.6)
 	if (missing(xlim)) {
 		xlim = mean + c(-1, 1) * zmax * sd
 	}
-	ymax = dnorm(mean, mean = mean, sd = sd)
-	if (missing(ylim)) {
-		ylim = c(0, 1.4 * ymax)
-	}
-	xdata = rep(xlim, each = 2)
-	ydata = rep(ylim, times = 2)
+	
+	Ddensity <-
+	   data_frame(
+	     x = seq(xlim[1], xlim[2], length.out = 500),
+	     density = dnorm(x, mean, sd),
+	     tail = ifelse(x <= q, 
+	                   paste0("P(X <= ", format(q, digits = 2), ") = ", round(pnorm(z), 3)),
+	                   paste0("P(X >  ", format(q, digits = 2), ") = ", round(1 - pnorm(z), 3))
+	     )
+	   )
+	
+	Dtext <- 
+	  data_frame(
+	    x = q,
+	    z = (q - mean)/sd,
+	    y1 = dnorm(mean, mean, sd),
+	    y2 = 1.1 * dnorm(mean, mean, sd),
+	    label = paste("z = ", round(z, 2))
+	  )
+	  
 
-	plot <- xyplot(ydata ~ xdata, xlim = xlim, ylim = ylim, 
-		xlab = "", ylab = "density", 
-		panel = function(x, y, ...) {
-			panel.mathdensity(dmath = dnorm, args = list(mean = mean, 
-			  sd = sd), lwd = 2, n = 100, col = "navy")
-			xs <- seq(xlim[1], q, by=diff(xlim)/500)
-			panel.xyplot(xs, dnorm(xs, mean, sd) , type='h')
-			if (dnorm(q, mean, sd) > 0.5 * ymax) {
-			  textloc = c(q, ymax * 0.2)
-			  textloc = c(q, 1.2 * ymax)
-			}
-			else {
-			  textloc = c(q, ymax * 0.9)
-			  textloc = c(q, 1.2 * ymax)
-			}
-      # vertical line
-			panel.segments(q, 0, q, grid::unit(ymax,'native') + grid::unit(0.6,'lines'), 
-			  col = vcol, lwd=vlwd)
-			#panel.segments(q, textloc[2] + 0.1 * ymax, q, 
-			#  ylim[2], col = "forestgreen")
-      # quantile
-			grid.text(x=q, y=grid::unit(ymax,'native') + grid::unit(1.8,'lines'),  
-                default.units='native',
-				paste(round(q, digits)), 
-				just = c('center','bottom'), gp=gpar(cex = 1.0))
-      # z score
-			grid.text(x=q, y=grid::unit(ymax,'native') + grid::unit(1.8,'lines'), default.units='native',
-			  paste("(z=", round(z, 3), ")", sep = ""), 
-				just = c('center','top'),  gp=gpar(cex = 0.9))
-      # arrow <- 
-			grid.lines( gp=gpar(lwd=1.5),
-				x=unit.c( grid::unit(q,'native'), grid::unit(q,'native') - grid::unit(2,'char') ),
-				y=grid::unit(ymax,'native') + grid::unit(.2,'lines'),
-				arrow=grid::arrow(angle=20,length=grid::unit(.75,'char'))
-				)
-      # left prob
-			grid.text(
-				x=grid::unit(q,'native') - grid::unit(3,'char'), 
-				y=grid::unit(ymax,'native') + grid::unit(.2,'lines'), default.units='native',
-				paste(round(p, digits), ""), 
-				just = c('right','bottom'),  gp=gpar(cex = 0.9))
-      # arrow ->
-			grid.lines( gp=gpar(lwd=1.5),
-				x=unit.c( grid::unit(q,'native'), grid::unit(q,'native') + grid::unit(2,'char') ),
-				y=grid::unit(ymax,'native') + grid::unit(.2,'lines'),
-				arrow=grid::arrow(angle=20,length=grid::unit(.75,'char'))
-				)
-      # left prob
-			grid.text(
-				x=grid::unit(q,'native') + grid::unit(3,'char'), 
-				y=grid::unit(ymax,'native') + grid::unit(.2,'lines'), default.units='native',
-				paste("", round(1 - p, digits)), 
-				just = c('left','bottom'),  gp=gpar(cex = 0.9))
-		}, ...)
+	res_plot <-
+	  ggplot(data = Ddensity, aes(x = x, y = density, group = tail, fill = tail)) +
+	    geom_area() + 
+	    geom_text(aes(x = x, y = y2, label = label), data = Dtext, 
+	              vjust = 0, hjust = 0.5, inherit.aes = FALSE) +
+	    geom_segment(aes(x = x, xend = x, y = 0, yend = y1), data = Dtext, inherit.aes = FALSE) +
+	    guides(fill = guide_legend(title = ""))
 
-	return(plot)
+	return(res_plot)
+	
 }
