0118222b3adf6ea30e2430cb3ae92f3eae3af36e
Randall Pruim <rpruim@gmail.com>
2017-07-19 15:02:02 -0400

    minor housecleaning

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,6 @@
 
-# tryCatch(utils::globalVariables(c('RStudioGD')), 
-#          error=function(e) message('Looks like you should update R.'))
-
 #' @details
-#' \code{.do.safe.call} avoids conflicts between named arguments and ... by taking named arguments 
+#' \code{.do.safe.call} avoids conflicts between named arguments and ... by taking named arguments
 #' preferentially.
 #'
 #' @rdname mosaic-internal
@@ -11,7 +8,7 @@
 #' @param what either a function or a non-empty character string naming the function to be called.
 #' @param args a list of arguments to the function call. The names attribute of args gives the argument names.
 #' @param quote a logical value indicating whether to quote the arguments.
-#' @param envir an environment within which to evaluate the call. 
+#' @param envir an environment within which to evaluate the call.
 #'	This will be most useful if what is a character string and the arguments are symbols or quoted expressions.
 #' @return The result of the (evaluated) function call.
 #'
@@ -30,35 +27,35 @@
 
 
 .try_requireNamespace <-function(package) {
-  available <- suppressMessages(suppressWarnings(sapply(package, 
-                                                        requireNamespace, quietly = TRUE, 
-                                                        character.only = TRUE, 
+  available <- suppressMessages(suppressWarnings(sapply(package,
+                                                        requireNamespace, quietly = TRUE,
+                                                        character.only = TRUE,
                                                         warn.conflicts = FALSE)))
   missing <- package[!available]
-  if (length(missing) > 0) 
-    stop("Missing packages.  Please retry after installing the following: ", 
-         paste(missing, collapse = ", "), 
+  if (length(missing) > 0)
+    stop("Missing packages.  Please retry after installing the following: ",
+         paste(missing, collapse = ", "),
          call. = FALSE)
 }
 
 .require_manipulate_namespace <-function() {
   if (! rstudio_is_available())
-    stop("RStudio required for manipulate.") 
-  if (! requireNamespace("manipulate", quietly=TRUE)) 
+    stop("RStudio required for manipulate.")
+  if (! requireNamespace("manipulate", quietly=TRUE))
     stop("Unable to locate the manipulate package.  Please install with `install.packages(\"manipulate\")'")
   requireNamespace("manipulate", quietly=TRUE)
 }
 
 #' Check whether RStudio is in use
-#' 
+#'
 #' This functions checks that RStudio is in use.  It will likely be removed
-#' from this package once the versions of RStudio in popular use rely on the 
+#' from this package once the versions of RStudio in popular use rely on the
 #' manipulate package on CRAN which will provide its own version.
-#' 
+#'
 #' @return a logical
-#' 
+#'
 #' @rdname rstudio
-#' 
+#'
 #' @export
 rstudio_is_available <- function() {
   identical(.Platform$GUI, "RStudio")
@@ -68,25 +65,3 @@ is_object_not_found_error <- function(e, name = "") {
   grepl("object", e$message) && grepl(name, e$message) && grepl("not found", e$message)
 }
 
-# f_new <- function (rhs, lhs = NULL, env = parent.frame()) {
-#   if (!is_lang(rhs) && !is.null(rhs)) {
-#     stop("`rhs` must be a language object", call. = FALSE)
-#   }
-#   if (!is_lang(lhs) && !is.null(lhs)) {
-#     stop("`lhs` must be a language object", call. = FALSE)
-#   }
-#   if (!is.environment(env)) {
-#     stop("`env` must be an environment", call. = FALSE)
-#   }
-#   if (is.null(lhs)) {
-#     f <- call_new("~", rhs)
-#   }
-#   else {
-#     f <- call_new("~", lhs, rhs)
-#   }
-#   structure(f, class = "formula", .Environment = env)
-# }
-# 
-# lazy2f <- function(x) {
-#   f_new(x$expr, env = x$env)
-# }
