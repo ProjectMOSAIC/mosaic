76699aeb724c384f33f6e8161adc13492c7bd2e3
Randall Pruim <rpruim@gmail.com>
2017-07-19 08:00:55 -0400

    surround = with space

--- R/aggregatingMeta.R
+++ R/aggregatingMeta.R
@@ -30,9 +30,9 @@ globalVariables(c("FUNCTION_TBD", "NA.RM", "OUTPUT.MULTIPLE"))
 #' @examples
 #' if (require(mosaicData)) {
 #'   foo <- aggregatingFunction1(base::mean)
-#'   foo( ~ length, data=KidsFeet)
+#'   foo( ~ length, data = KidsFeet)
 #'   base::mean(KidsFeet$length)
-#'   foo(length ~ sex, data=KidsFeet)
+#'   foo(length ~ sex, data = KidsFeet)
 #' } 
 #' @export
 # 
@@ -42,8 +42,8 @@ globalVariables(c("FUNCTION_TBD", "NA.RM", "OUTPUT.MULTIPLE"))
 
 aggregatingFunction1 <- 
   function(fun, 
-           output.multiple=FALSE, 
-           envir=parent.frame(), 
+           output.multiple = FALSE, 
+           envir = parent.frame(), 
            na.rm = getOption("na.rm", FALSE),
            style = c("formula1st", "formula", "flexible")
   ) {
@@ -74,19 +74,19 @@ aggregatingFunction1 <-
             )
           ## See issue #531 for problem with using lazy_formula.
           if (is.null(lazy_formula) || ! inherits(lazy_formula$expr, "formula")) {
-            lazy_formula <- structure(list(expr=subst_x, env=pframe), class="lazy")
+            lazy_formula <- structure(list(expr = subst_x, env = pframe), class = "lazy")
           }
           if (is.null(data)) {
             result <-
               tryCatch(FUNCTION_TBD(x, ..., na.rm = na.rm), 
-                       error=function(e) {e} , 
-                       warning=function(w) {w}) 
+                       error = function(e) {e} , 
+                       warning = function(w) {w}) 
             if (! inherits(result, c("error", "warning")))  return(result) 
             data <- parent.frame()
           }
           formula <- formularise(lazy_formula, parent.frame(2)) 
-          formula <- mosaic_formula_q(formula, groups=groups, max.slots=3) 
-          maggregate(formula, data=data, FUN = FUNCTION_TBD, na.rm = na.rm, ..., .multiple = output.multiple)
+          formula <- mosaic_formula_q(formula, groups = groups, max.slots = 3) 
+          maggregate(formula, data = data, FUN = FUNCTION_TBD, na.rm = na.rm, ..., .multiple = output.multiple)
         },
       formula = 
         function(
@@ -97,13 +97,13 @@ aggregatingFunction1 <-
         {
           if (is.null(data)) {
             result <-
-              tryCatch(FUNCTION_TBD(x, ..., na.rm = na.rm), error=function(e) {e} , warning=function(w) {w} ) 
+              tryCatch(FUNCTION_TBD(x, ..., na.rm = na.rm), error = function(e) {e} , warning = function(w) {w} ) 
             if (! inherits(result, c("error", "warning")))  return(result) 
             data <- parent.frame()
           }
           if (! inherits(x, "formula")) stop("`x' must be a formula")
-          formula <- mosaic_formula_q(x, groups=groups, max.slots=3) 
-          maggregate(formula, data=data, FUN = FUNCTION_TBD, ..., .multiple = output.multiple)
+          formula <- mosaic_formula_q(x, groups = groups, max.slots = 3) 
+          maggregate(formula, data = data, FUN = FUNCTION_TBD, ..., .multiple = output.multiple)
         },
       
       formula1st = 
@@ -115,7 +115,7 @@ aggregatingFunction1 <-
         {
           if (lazyeval::is_formula(x)) {
             if (is.null(data)) data <- lazyeval::f_env(x)
-            formula <- mosaic_formula_q(x, groups=groups, max.slots=3) 
+            formula <- mosaic_formula_q(x, groups = groups, max.slots = 3) 
             return(maggregate(formula, data = data, FUN = FUNCTION_TBD, ..., 
                               na.rm = na.rm,
                               .multiple = OUTPUT.MULTIPLE))
@@ -139,9 +139,9 @@ aggregatingFunction1 <-
 
 
 # #' @export
-# aggregatingFunction1or2 <- function(fun, input.multiple=FALSE, output.multiple=FALSE, 
-#                                      envir=parent.frame(), na.rm=getOption("na.rm",FALSE)) {
-#   result <- function(x, ..., data, groups=NULL) {
+# aggregatingFunction1or2 <- function(fun, input.multiple = FALSE, output.multiple = FALSE, 
+#                                      envir = parent.frame(), na.rm = getOption("na.rm",FALSE)) {
+#   result <- function(x, ..., data, groups = NULL) {
 #     orig.call <- match.call()
 #     fun.call <- orig.call 
 #     fun.call[[1]] <- substitute(..fun..)
@@ -151,11 +151,11 @@ aggregatingFunction1 <-
 #     missingData <- FALSE  
 #     if (missing(data)) {
 #       missingData <- TRUE
-#       data=parent.frame()
+#       data = parent.frame()
 #       
-#       result <- tryCatch(eval(fun.call, envir=parent.frame()) , 
-#                           error=function(e) {e} ,
-#                           warning=function(w) {w}) 
+#       result <- tryCatch(eval(fun.call, envir = parent.frame()) , 
+#                           error = function(e) {e} ,
+#                           warning = function(w) {w}) 
 #       if ( ! inherits(result, "warning") && ! inherits(result,"error")) {
 #         return(result) 
 #       }
@@ -169,7 +169,7 @@ aggregatingFunction1 <-
 #         fun.call[['data']] <- NULL
 #       }
 #       if (input.multiple) {
-#         result <- tryCatch(eval(fun.call, envir=data, enclos=parent.frame()),
+#         result <- tryCatch(eval(fun.call, envir = data, enclos = parent.frame()),
 #                             error = function(e) {e},
 #                             warning = function(w) {w}) 
 #         if ( ! inherits(result, "warning") && ! inherits(result,"error")) {
@@ -178,8 +178,8 @@ aggregatingFunction1 <-
 #       }
 #       
 #       x <- eval(substitute( 
-#         mosaic_formula_q(.x, groups=quote(groups)), 
-#         list(.x=substitute(x) , .g=substitute(groups))
+#         mosaic_formula_q(.x, groups = quote(groups)), 
+#         list(.x = substitute(x) , .g = substitute(groups))
 #       ) )
 #       
 #       if ("groups" %in% names(maggregate.call)) maggregate.call[['groups']] <- NULL
@@ -195,9 +195,9 @@ aggregatingFunction1 <-
 #     maggregate.call$.multiple <- output.multiple
 #     maggregate.call$na.rm <- substitute(na.rm)
 #     #    print(maggregate.call)
-#     return(eval(maggregate.call, envir=envir))
+#     return(eval(maggregate.call, envir = envir))
 #   }
-#   formals(result) <- c(formals(result), ..fun.. = substitute(fun), na.rm=substitute(na.rm))
+#   formals(result) <- c(formals(result), ..fun.. = substitute(fun), na.rm = substitute(na.rm))
 #   return(result)
 # }
 
@@ -287,7 +287,7 @@ aggregatingFunction1or2 <-
 #' @examples
 #' if(require(mosaicData)) {
 #'   foo <- aggregatingFunction2(stats::cor)
-#'   foo(length ~ width, data=KidsFeet)
+#'   foo(length ~ width, data = KidsFeet)
 #'    stats::cor(KidsFeet$length, KidsFeet$width)
 #' }
 #' @export
@@ -322,7 +322,7 @@ aggregatingFunction2 <- function(fun) {
 # old version -- keep until we establish that new version is working properly.
 
 # aggregatingFunction2 <- function(fun) {
-#   result <- function(x, y=NULL, ..., data=parent.frame()) { # , ..fun.. = fun) {
+#   result <- function(x, y = NULL, ..., data = parent.frame()) { # , ..fun.. = fun) {
 #     orig.call <- match.call()
 #     mosaic.call <- orig.call 
 #     mosaic.call[[1]] <- fun
@@ -332,15 +332,15 @@ aggregatingFunction2 <- function(fun) {
 #       if (!'data' %in% names(formals(fun)) && ! "..." %in% names(formals(fun))) {
 #         if("data" %in% names(mosaic.call)) mosaic.call[["data"]] <- NULL  # in case original function didn't have ...
 #       }
-#       return (eval(mosaic.call , data, enclos=parent.frame()))
+#       return (eval(mosaic.call , data, enclos = parent.frame()))
 #     }
 #     
 #     # message("Using mosaic super powers!")
 #     formula <- eval(orig.call$x, parent.frame())
 #     mosaic.call[['data']] <- NULL
 #     # if (is.null(mosaic.call[['data']])) mosaic.call[['data']] <- quote(parent.frame())
-#     mosaic.call$x <- eval(lhs(formula), envir=data, enclos=parent.frame())
-#     mosaic.call$y <- eval(rhs(formula), envir=data, enclos=parent.frame())
+#     mosaic.call$x <- eval(lhs(formula), envir = data, enclos = parent.frame())
+#     mosaic.call$y <- eval(rhs(formula), envir = data, enclos = parent.frame())
 #     if (! "..." %in% names(formals(orig.call))) {
 #       for (n in setdiff(names(mosaic.call), names(formals(fun)))) {
 #         if (n != "") mosaic.call[[n]] <- NULL
@@ -367,7 +367,7 @@ aggregatingFunction2 <- function(fun) {
 #' @param y a numeric vector or a formula
 #' @param groups a grouping variable, typically a name of a variable in \code{data}
 #' @param data a data frame in which to evaluate formulas (or bare names).
-#' Note that the default is \code{data=parent.frame()}.  This makes it convenient to
+#' Note that the default is \code{data = parent.frame()}.  This makes it convenient to
 #' use this function interactively by treating the working envionment as if it were 
 #' a data frame.  But this may not be appropriate for programming uses.  
 #' When programming, it is best to use an explicit \code{data} argument
@@ -450,11 +450,11 @@ prod <- aggregatingFunction1(base::prod)
 sum <- aggregatingFunction1(base::sum)
 #' @rdname aggregating
 #' @export
-favstats <- aggregatingFunction1(fav_stats, output.multiple=TRUE, na.rm=TRUE)
+favstats <- aggregatingFunction1(fav_stats, output.multiple = TRUE, na.rm = TRUE)
 #' @rdname aggregating
 #' @export
-quantile <- aggregatingFunction1(stats::quantile, output.multiple=TRUE, 
-                                 na.rm=getOption("na.rm", FALSE))
+quantile <- aggregatingFunction1(stats::quantile, output.multiple = TRUE, 
+                                 na.rm = getOption("na.rm", FALSE))
 #' @rdname aggregating
 #' @export
 var <- aggregatingFunction1or2(stats::var)
@@ -465,24 +465,24 @@ cor <- aggregatingFunction2(stats::cor)
 #' 
 #' @examples
 #' mean(HELPrct$age)
-#' mean( ~ age, data=HELPrct)
-#' mean( ~ drugrisk, na.rm=TRUE, data=HELPrct)
-#' mean(age ~ shuffle(sex), data=HELPrct)
-#' mean(age ~ shuffle(sex), data=HELPrct, .format="table")
+#' mean( ~ age, data = HELPrct)
+#' mean( ~ drugrisk, na.rm = TRUE, data = HELPrct)
+#' mean(age ~ shuffle(sex), data = HELPrct)
+#' mean(age ~ shuffle(sex), data = HELPrct, .format = "table")
 #' # wrap in data.frame() to auto-convert awkward variable names
-#' data.frame(mean(age ~ shuffle(sex), data=HELPrct, .format="table"))
-#' mean(age ~ sex + substance, data=HELPrct)
-#' mean( ~ age | sex + substance, data=HELPrct)
-#' mean( ~ sqrt(age), data=HELPrct)
-#' sum( ~ age, data=HELPrct)
+#' data.frame(mean(age ~ shuffle(sex), data = HELPrct, .format = "table"))
+#' mean(age ~ sex + substance, data = HELPrct)
+#' mean( ~ age | sex + substance, data = HELPrct)
+#' mean( ~ sqrt(age), data = HELPrct)
+#' sum( ~ age, data = HELPrct)
 #' sd(HELPrct$age)
-#' sd( ~ age, data=HELPrct)
-#' sd(age ~ sex + substance, data=HELPrct)
+#' sd( ~ age, data = HELPrct)
+#' sd(age ~ sex + substance, data = HELPrct)
 #' var(HELPrct$age)
-#' var( ~ age, data=HELPrct)
-#' var(age ~ sex + substance, data=HELPrct)
-#' IQR(width ~ sex, data=KidsFeet)
-#' iqr(width ~ sex, data=KidsFeet)
+#' var( ~ age, data = HELPrct)
+#' var(age ~ sex + substance, data = HELPrct)
+#' IQR(width ~ sex, data = KidsFeet)
+#' iqr(width ~ sex, data = KidsFeet)
 #' favstats(width ~ sex, data = KidsFeet)
 #' 
 #' cor(length ~ width, data = KidsFeet)
@@ -502,8 +502,8 @@ cov <- aggregatingFunction2(stats::cov)
 #' @seealso \code{\link{mad}}
 #' @rdname MAD_
 #' @export
-MAD_ <- function(x, ..., na.rm=getOption("na.omit", FALSE)) {
-  SAD_(x, ..., na.rm=na.rm) / (length(x) + length(list(...)))
+MAD_ <- function(x, ..., na.rm = getOption("na.omit", FALSE)) {
+  SAD_(x, ..., na.rm = na.rm) / (length(x) + length(list(...)))
 }
 
 #' @rdname MAD_
@@ -530,7 +530,7 @@ SAD_ <- function(x, ..., na.rm = getOption("na.omit", FALSE)) {
 #'   calculaing.
 #' @param groups a grouping variable, typically a name of a variable in \code{data}
 #' @param data a data frame in which to evaluate formulas (or bare names).
-#'   Note that the default is \code{data=parent.frame()}.  This makes it convenient to
+#'   Note that the default is \code{data = parent.frame()}.  This makes it convenient to
 #'   use this function interactively by treating the working envionment as if it were 
 #'   a data frame.  But this may not be appropriate for programming uses.  
 #'   When programming, it is best to use an explicit \code{data} argument
@@ -543,7 +543,7 @@ SAD_ <- function(x, ..., na.rm = getOption("na.omit", FALSE)) {
 #' @examples
 #' SAD(1:3)
 #' MAD(1:3)
-#' MAD(~eruptions, data=faithful)
+#' MAD(~eruptions, data = faithful)
 
 MAD <- aggregatingFunction1(MAD_)
 
