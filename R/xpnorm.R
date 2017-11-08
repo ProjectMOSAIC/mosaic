97847102d45c62229632b0a26872c47536d52516
Randall Pruim <rpruim@gmail.com>
2017-11-08 16:24:37 -0500

    add example showing how to return plot and modify it.

--- R/xpnorm.R
+++ R/xpnorm.R
@@ -39,11 +39,15 @@ utils::globalVariables(
 #' @examples
 #' xpnorm(650, 500, 100)
 #' xqnorm(.75, 500, 100)
+#' xpnorm(-3:3, return = "plot", system = "gg") %>% 
+#'   gf_labs(title = "My Plot", x = "") %>% 
+#'   gf_theme(theme_bw())
+#' 
 #' \dontrun{
 #' if (rstudio_is_available() & require(manipulate)) {
-#'   manipulate(xpnorm(score, 500, 100, verbose=verbose),
-#'     score = slider(200,800),
-#' 	   verbose = checkbox(TRUE, label="Verbose Output")
+#'   manipulate(xpnorm(score, 500, 100, verbose = verbose),
+#'     score = slider(200, 800),
+#' 	   verbose = checkbox(TRUE, label = "Verbose Output")
 #'   )
 #' }
 #' }
