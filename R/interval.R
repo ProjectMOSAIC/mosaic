18c6e2ba8ef9d9ad992b425323600ceae2ae3607
Randall Pruim <rpruim@gmail.com>
2018-05-28 16:36:55 -0400

    add stat method for class uneval

--- R/interval.R
+++ R/interval.R
@@ -143,6 +143,20 @@ stat.htest <- function(x,...) {
 	x $ statistic
 }
 
+# This is added for compatibility with ggplo2 version 2.3
+
+#' @rdname interval
+#' @export
+#' 
+stat.uneval <- function(x, ...) {
+  if (packageVersion("ggplot2") <= "2.2.1") {
+    stop("ggplot2 > 2.2.1 required for ggplot2::stat().")
+  } else {
+	  ggplot2::stat(x, ...)
+  }
+}
+
+
 #' Extract r-squared value
 #' 
 #' Attempts to extract an r-squared value from a model or model-like object.
