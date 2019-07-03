6f52998a26324bc50938ef359e1f6fc7199739c8
Jason Klaassen <jwk24@rstudio.cs.calvin.edu>
2019-07-03 18:59:12 +0000

    Finished testing FrequencyPolygon and removing the lazyeval.

--- tests/testthat/test-frequencyPolygon.R
+++ tests/testthat/test-frequencyPolygon.R
@@ -1,12 +1,16 @@
-require(mosaic)
-require(mosaicData)
+
 # Examples
 # freqpolygon(~age | substance, data=HELPrct, v=35)
 # freqpolygon(~age, data=HELPrct, labels=TRUE, type='count')
 # freqpolygon(~age | substance, data=HELPrct, groups=sex)
 # freqpolygon(~age | substance, data=HELPrct, groups=sex, ylim=c(0,0.11))
-testthat::test_that("Returns a trellis object",{
-  context("Histograms")
-  p <- ggplot(mtcars, aes(disp)) + geom_histogram()
-  vdiffr::expect_doppelganger("FirstExample", p)
+testthat::test_that("Examples are working",{
+ 
+  vdiffr::expect_doppelganger("FirstExample", freqpolygon(~age | substance, data=HELPrct, v=35))
+  
+  vdiffr::expect_doppelganger("SecondExample", freqpolygon(~age, data=HELPrct, labels=TRUE, type='count'))
+  
+  vdiffr::expect_doppelganger("ThirdExample", freqpolygon(~age | substance, data=HELPrct, groups=sex))
+  
+  vdiffr::expect_doppelganger("FourthExample", freqpolygon(~age | substance, data=HELPrct, groups=sex, ylim=c(0,0.11)))
 })
