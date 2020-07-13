385d173c1fcbfa6b9528b02796fc39f81101849e
Randall Pruim <rpuim@gmail.com>
2020-07-13 15:14:39 -0400

    add ggridges to packages that are attached on load.

--- R/zzz.R
+++ R/zzz.R
@@ -46,4 +46,4 @@ is_attached <- function(x) {
 
 
 pkgs_to_attach <-
-  c("lattice", "ggplot2", "Matrix", "dplyr", "ggstance", "ggformula", "mosaicData")
+  c("lattice", "ggplot2", "Matrix", "dplyr", "ggstance", "ggformula", "mosaicData", "ggridges")
