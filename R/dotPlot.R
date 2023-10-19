2b6f71f19ad969256b300e65b7d9629842076063
Randall Pruim <rpruim@gmail.com>
2023-10-18 21:01:13 -0400

    whitespace edits

--- R/dotPlot.R
+++ R/dotPlot.R
@@ -107,10 +107,11 @@ panel.dotPlot <-
                 yvals <- 1:(h$counts[bin])
                 xvals <- rep( h$mids[bin], h$counts[bin] )
                 grid.points( 
-                  size= cex * grid::unit( 0.9 / current.panel.limits()$ylim[2], 
-                                    "snpc"),  
+                  size = cex * grid::unit(0.9 / current.panel.limits()$ylim[2], 
+                                          "snpc"),  
                   pch = pch, 
-                  gp = gpar(fill = col, alpha = alpha, col = col, lty = lty, lwd = lwd), 
+                  gp = gpar(fill = col, alpha = alpha, col = col, 
+                            lty = lty, lwd = lwd), 
 				  		    x = xvals,
                   y = yvals - 0.5, 
 						default.units = "native")
