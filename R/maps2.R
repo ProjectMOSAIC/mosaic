785e4b901bc3dd63595488ae3eed10c90fc30b4b
Randall Pruim <rpruim@gmail.com>
2023-10-30 09:52:00 -0400

    rebuild documentation

--- R/maps2.R
+++ R/maps2.R
@@ -363,23 +363,23 @@ CIAdata <- function (name = NULL) {
 #' information (ex: `long` and `lat`)
 #' @examples
 #'
-# gdontrun{
-# if(require(maptools)) {
-#   data(wrld_simpl)
-#   worldmap <- sp2df(wrld_simpl)
-# }
-# 
-# if ( require(ggplot2) && require(maptools) ) {
-#   data(wrld_simpl)
-#   World <- sp2df(wrld_simpl)
-#   World2 <- merge(World, Countries, by.x="NAME", by.y="maptools", all.y=FALSE)
-#   Mdata <- merge(Alcohol, World2, by.x="country", by.y="gapminder", all.y=FALSE)
-#   Mdata <- Mdata[order(Mdata$order),]
-#   qplot( x=long, y=lat, fill=ntiles(alcohol,5),
-#          data=subset(Mdata, year==2008), group = group,
-#                      geom="polygon")
-# }
-# }
+#' gdontrun{
+#' if(require(maptools)) {
+#'   data(wrld_simpl)
+#'   worldmap <- sp2df(wrld_simpl)
+#' }
+#' 
+#' if ( require(ggplot2) && require(maptools) ) {
+#'   data(wrld_simpl)
+#'   World <- sp2df(wrld_simpl)
+#'   World2 <- merge(World, Countries, by.x="NAME", by.y="maptools", all.y=FALSE)
+#'   Mdata <- merge(Alcohol, World2, by.x="country", by.y="gapminder", all.y=FALSE)
+#'   Mdata <- Mdata[order(Mdata$order),]
+#'   qplot( x=long, y=lat, fill=ntiles(alcohol,5),
+#'          data=subset(Mdata, year==2008), group = group,
+#'                      geom="polygon")
+#' }
+#' }
 #' @export
 sp2df <- function (map, ...) 
 {
